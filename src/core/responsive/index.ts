d30ce590d67a564e3c46c99f670a5b7ab3d710d1
ZhuChongLin <8210856@qq.com>
2024-10-22 22:40:21 +0800

    feat(core/responsive): 添加依赖追踪功能
    - 新增 Dep 和 track 函数的导出
    -从 track-dependencies.js 模块引入相关功能

--- src/core/responsive/index.ts
+++ src/core/responsive/index.ts
@@ -1,2 +1,3 @@
 export { ref, isProxy, isPlainProxy } from './proxy.js'
 export { watch, watchReturnSource, watchDep, watchOnce } from './watch.js'
+export { Dep, track } from './track-dependencies.js'
