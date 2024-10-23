ead74db5468009292cf979394597165e92ad78ec
ZhuChongLin <8210856@qq.com>
2024-10-23 19:22:01 +0800

    refactor(watch): 重构监听器类型定义
    - 优化了类型定义，简化了 ExtractRefType 等类型的实现
    - 新增了 ExtractWatchSourceType、InferArrayType 等实用类型
    - 改进了 WatchCallback 类型的定义，增加了 origin 参数
    - 删除了未使用的 IS_PLAIN_PROXY_SYMBOL 和 IS_PROXY_SYMBOL 引用

--- src/types/watch.ts
+++ src/types/watch.ts
@@ -1,67 +1,54 @@
-import type { IS_PLAIN_PROXY_SYMBOL, IS_PROXY_SYMBOL } from '../core/responsive/proxy'
-// 提取函数源返回值类型
-type ExtractArraySourceType<T, D, P = true> = ExtractRefType<
-  T,
-  T extends Array<infer R> ? ExtractRefType<R, R, P> : D
->
-// 提取函数源类型
-type ExtractFnType<T, D, P = true> = T extends (...args: any[]) => infer R
-  ? ExtractRefType<R, ExtractArraySourceType<R, R, P>, P>
-  : D
-// 提取代理类型
-type ExtractRefType<T, D, P = true> =
-  T extends Vitarx.Ref<T>
-    ? P extends true
-      ? T extends Vitarx.PlainProxy<any>
-        ? T['value']
-        : T
-      : T
-    : D
-// 创建一个联合类型，它包含了改变的值类型
-export type ChangeValue<T> = ExtractRefType<T, ExtractFnType<T, ExtractArraySourceType<T, T>>>
-// 用于检查数组中的元素类型是否有一个符合 Vitarx.Ref<T> 是则用number做为索引
-type HasRefElement<T, D> = T extends (infer U)[] ? (U extends Vitarx.Ref<U> ? number : D) : D
-// 排除代理符号
-type ExcludeSymbol<T> = Exclude<T, typeof IS_PLAIN_PROXY_SYMBOL | typeof IS_PROXY_SYMBOL>
-// 提取数组中的元素索引
-type ExtractArrayIndexType<T, D> =
-  T extends Array<any> ? ExtractRefIndexType<T, HasRefElement<T, string | number | symbol>> : D
-// 提取函数返回源索引
-type ExtractFnIndexType<T, D> = T extends (...args: any[]) => infer R
-  ? ExtractArrayIndexType<R, ExtractRefIndexType<R, string | number | symbol>>
-  : D
+// 提取监听源类型
+export type ExtractWatchSourceType<T> = T extends AnyFunction ? ReturnType<T> : T
+// 推出数组元素类型
+export type InferArrayType<T, D = never> = T extends Array<infer R> ? R : D
+// 推出代理类型，否则返回默认值
+export type ExtractRefType<T, D> =
+  T extends Vitarx.PlainProxy<any> ? T['value'] : T extends Vitarx.Ref<T> ? T : D
+// 创建一个联合类型，它包含了可能改变的值类型
+export type ExtractValueType<T> = ExtractRefType<T, InferArrayType<T, T>>
 // 提取代理类型的索引
-type ExtractRefIndexType<T, D> = T extends Vitarx.Ref<T> ? ExcludeSymbol<keyof T> : D
-export type ExtractIndex<T> = ExtractRefIndexType<
+export type ExtractRefIndexType<T, D> = T extends Vitarx.Ref<T> ? keyof Vitarx.UnRef<T> : D
+// 提取索引类型
+export type ExtractIndexType<T, D = string | number | symbol> =
+  T extends Array<any> ? number : ExtractRefIndexType<T, D>
+// 提取父节点类型
+export type ExtractOriginType<
   T,
-  ExtractFnIndexType<T, ExtractArrayIndexType<T, string | number | symbol>>
->
-
-// 任意函数
-export type AnyFunction = (...args: any[]) => any
-// 改变的键或索引
-export type ChangeIndex = Array<string | symbol | number>
+  D =
+    | Vitarx.Ref<{
+        [k: string | number | symbol]: any
+      }>
+    | undefined
+> = T extends Vitarx.PlainProxy<any> ? T : T extends Vitarx.Ref<T> ? T : InferArrayType<T, D>
 /**
  * 回调函数类型
  *
  * @template T - 监听的目标
- * @template V - 回调函数类型
- * @template I - 变更的键或索引
- * @param {V} newValue - 新值
- * @param {V} oldValue - 旧值
- * @param {I} index - 变更的键或索引，当同时监听多个不同源对象时会是number类型，表示其在数组中的索引
- * @param {Vitarx.PlainProxy<any> | Vitarx.ObjectProxy<object>} source - 源对象
  */
-export type WatchCallback<
-  T,
-  V = ChangeValue<T>,
-  I extends number | string | symbol = ExtractIndex<T>
-> = (
-  newValue: V,
-  oldValue: V,
+export type Callback<T> = WatchCallback<
+  ExtractValueType<ExtractWatchSourceType<T>>,
+  Vitarx.UnRef<ExtractValueType<ExtractWatchSourceType<T>>>,
+  ExtractIndexType<ExtractWatchSourceType<T>>,
+  ExtractOriginType<ExtractWatchSourceType<T>>
+>
+/**
+ * 回调函数类型，接受四个参数：
+ * - newValue - 新值
+ * - oldValue - 旧值
+ * - index - 变化的索引，如果监听的是数组则返回数组索引，监听的是对象返回属性名
+ * - origin - 监听源，如果监听的是数组，则返回数组，监听的是对象，则返回对象，监听的是对象属性则返回属性所在的对象！如果属性所在的对象被删除了，则返回undefined
+ */
+export type WatchCallback<NV = any, OV = any, I = any, O = any> = (
+  newValue: NV,
+  oldValue: OV,
   index: I,
-  source: Vitarx.PlainProxy<any> | Vitarx.ObjectProxy<object>
+  origin: O
 ) => void
+// 任意函数
+export type AnyFunction = (...args: any[]) => any
+// 改变索引
+export type ChangeIndex = Array<string | symbol | number>
 
 /**
  * ## 监听器选项
