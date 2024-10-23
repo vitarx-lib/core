dfd20766dfc2e5d6b326b9f0c935e4121e8deca1
ZhuChongLin <8210856@qq.com>
2024-10-23 19:22:44 +0800

    feat(core/responsive): 新增 trigger 函数
    - 从 watch.js 中导出 trigger 函数，使其可在核心响应式模块中使用

--- src/core/responsive/index.ts
+++ src/core/responsive/index.ts
@@ -1,3 +1,3 @@
 export { ref, isProxy, isPlainProxy } from './proxy.js'
-export { watch, watchReturnSource, watchDep, watchOnce } from './watch.js'
+export { watch, watchReturnSource, watchDep, watchOnce, trigger } from './watch.js'
 export { Dep, track } from './track-dependencies.js'
