c31cf43f00baec5b5724ac9de1ba4eadd0f491e1
ZhuChongLin <8210856@qq.com>
2024-10-24 21:08:16 +0800

    feat(core/responsive): 更新导出内容
    - 从 proxy.js 中导出新增的 isRef 和 reactive 函数
    - 从 watch.js 中导出更新后的 watch、watchFunc、watchKeys 函数
    - 移除 watchReturnSource 函数的导出

--- src/core/responsive/index.ts
+++ src/core/responsive/index.ts
@@ -1,3 +1,3 @@
-export { ref, isProxy, isPlainProxy } from './proxy.js'
-export { watch, watchReturnSource, watchDep, watchOnce, trigger } from './watch.js'
+export { ref, isProxy, isRef, reactive } from './proxy.js'
+export { watch, watchFunc, watchDep, watchOnce, watchKeys, trigger } from './watch.js'
 export { Dep, track } from './track-dependencies.js'
