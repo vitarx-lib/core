41eb5e6f49a6ac66b74e1128bb6c2bd2bf936b24
ZhuChongLin <8210856@qq.com>
2024-10-25 02:30:38 +0800

    feat(core/responsive): 添加 computed导出
    - 在 src/core/responsive/index.ts 中导出 computed模块的所有内容
    - 此修改增加了对 computed 功能的支持，进一步完善了响应式系统

--- src/core/responsive/index.ts
+++ src/core/responsive/index.ts
@@ -1,3 +1,4 @@
 export { ref, isProxy, isRef, reactive } from './proxy.js'
 export { watch, watchFunc, watchDep, watchOnce, watchKeys, trigger } from './watch.js'
 export { Dep, track } from './track-dependencies.js'
+export * from './computed.js'
