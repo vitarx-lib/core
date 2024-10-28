617e01ff155180ebf4898bd20da1f6cfbe958ae4
ZhuChongLin <8210856@qq.com>
2024-10-29 00:12:34 +0800

    feat(core): 添加代理模式相关常量
    - 新增 PROXY_SYMBOL、PROXY_DEEP_SYMBOL 和 PROXY_DESTROYED_METHOD_SYMBOL 常量
    - 这些常量用于代理模式的实现，包括深度代理和代理对象的销毁

---
+++ src/core/variable/constants.ts
@@ -0,0 +1,5 @@
+export const PROXY_SYMBOL = Symbol('VITARX_PROXY_SYMBOL')
+// 深度代理标识符
+export const PROXY_DEEP_SYMBOL = Symbol('VITARX_PROXY_DEEP_SYMBOL')
+// 销毁方法标识符，用于销毁代理对象
+export const PROXY_DESTROYED_METHOD_SYMBOL = Symbol('PROXY_DESTROYED_METHOD_SYMBOL')
