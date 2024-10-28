9d21946ea4255c22ed0fc798a524b356f4009d0a
ZhuChongLin <8210856@qq.com>
2024-10-29 00:14:40 +0800

    feat(core): 添加 Ref 引用类型
    - 实现 Ref 类，用于创建引用类型对象，可监听其变化
    - 添加 ref 函数，简化 Ref 对象的创建- 实现 isRef 函数，用于检测对象是否为 Ref 类型
    - 添加 unRef 函数，用于解除 Ref 对象，返回其值
    - Ref 类包含 value属性，用于获取和设置引用值
    - 支持惰性代理，当访问对象属性时自动转换为响应式对象
    - 添加触发更新事件的功能，用于通知观察者

---
+++ src/core/variable/ref.ts
@@ -0,0 +1,146 @@
+import { isObject } from '../../utils'
+import { isProxy, ProxySymbol } from './helper.js'
+import { createReactive } from './reactive.js'
+import { Observers } from '../observers'
+import { PROXY_DEEP_SYMBOL, PROXY_SYMBOL } from './constants.js'
+
+/** 解除响应式对象 */
+export type UnRef<T> = T extends Ref<any> ? T['value'] : T
+
+/**
+ * # `Ref`对象，用于引用一个值，并可以监听其变化
+ *
+ * @template T - 任意类型
+ * @remarks
+ * 该对象的`value`属性是响应式的，当其值发生变化时，会触发`watch`监听器的回调函数。
+ *
+ * 该对象的`value`属性可以是任意类型，但需注意的是，当目标值是一个响应式对象时，且深度代理为true时，这会抛出一个警告，并且不会监控其变化。
+ *
+ * @example
+ * ```ts
+ * const count = new Ref(0) // 或调用助手函数 ref(0)
+ * count.value++ // count 的值变为1
+ * ```
+ */
+export class Ref<T> implements ProxySymbol {
+  /** 目标变量 */
+  #target: T
+  /** 是否深度代理 */
+  readonly #deep: boolean
+
+  /**
+   * 构造函数
+   *
+   * @param target - 任意值
+   * @param deep - 是否深度代理，默认为true
+   */
+  constructor(target: T, deep: boolean = true) {
+    this.#target = target
+    this.#deep = deep
+    this.#detectProxy(target)
+  }
+
+  /** 获取目标变量 */
+  get value(): T {
+    // 惰性代理子对象
+    if (this.#deep && isObject(this.#target) && !isProxy(this.#target)) {
+      this.#target = createReactive(this.#target, this.#deep, this.trigger.bind(this))
+    }
+    // 返回目标变量
+    return this.#target
+  }
+
+  /** 修改目标变量 */
+  set value(newValue: T) {
+    if (newValue !== this.#target) {
+      this.#detectProxy(newValue)
+      this.#target = newValue
+      this.trigger()
+    }
+  }
+
+  // 代理标识
+  get [PROXY_SYMBOL](): true {
+    return true
+  }
+
+  // 深度代理标识
+  get [PROXY_DEEP_SYMBOL](): boolean {
+    return this.#deep
+  }
+
+  // 定义当对象需要转换成原始值时的行为
+  [Symbol.toPrimitive](hint: any) {
+    switch (hint) {
+      case 'number':
+        return this.value
+      case 'string':
+        return this.toString()
+      case 'default':
+        return this.value
+    }
+  }
+
+  /**
+   * 触发更新事件
+   *
+   * @private
+   */
+  trigger() {
+    Observers.trigger(this, 'value' as any)
+  }
+
+  /**
+   * 将引用的目标值转换为字符串
+   *
+   * 如果目标值有`toString`方法，则会返回目标值的字符串形式，否则返回[Object Ref<`typeof target`>]。
+   *
+   * @override
+   */
+  toString() {
+    if (this.#target?.toString) {
+      return this.#target.toString()
+    } else {
+      return `[Object Ref<${typeof this.#target}>]`
+    }
+  }
+
+  /** 检测代理 */
+  #detectProxy(value: any) {
+    if (this.#deep && isProxy(value)) {
+      console.warn(
+        `[Vitarx.Ref][WARN]：当deep属性为true时，Ref对象的引用值不应该是代理对象（Ref|Reactive）。`
+      )
+    }
+  }
+}
+
+/**
+ * 判断是否为 Ref 对象
+ *
+ * @param val
+ */
+export function isRef(val: any): val is Ref<any> {
+  return val instanceof Ref
+}
+
+/**
+ * 解除Ref对象，返回其value属性
+ *
+ * @template T - 任意类型
+ * @param ref - 任意值或`Ref`对象
+ * @return {T} 任意值或`Ref`对象的value属性
+ */
+export function unRef<T>(ref: T | Ref<T>): T {
+  return isRef(ref) ? ref.value : ref
+}
+
+/**
+ * 创建一个Ref对象
+ *
+ * @param value - 目标值
+ * @param deep - 是否深度代理，当值为object类型时有效
+ */
+export function ref<T>(value: T, deep: boolean = true): Ref<T> {
+  return new Ref(value, deep)
+}
