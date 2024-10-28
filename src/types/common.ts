9a1ff1542b77da3c9491dedc5a494ad9cc333d0d
ZhuChongLin <8210856@qq.com>
2024-10-29 00:12:23 +0800

    refactor(types): 优化基本类型定义
    - 重命名 AnyObject为 AnyRecord，更准确地表示对象类型
    - 新增 AnyCallback 类型，用于表示任意回调函数
    - 新增 AnyMap、AnyWeakMap、AnyWeakSet、AnySet 类型，覆盖各种集合类型
    - 新增 AnyCollection 类型，统一表示所有集合类型
    - 更新 AnyObject 类型，包含所有非函数的对象类型

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,8 +1,23 @@
 // 任意函数
 export type AnyFunction = (...args: any[]) => any
-export type AnyObject = Record<string | symbol, any>
+// 任意回调函数
+export type AnyCallback = AnyFunction
+// 任意对象
+export type AnyRecord = Record<any, any>
+// 任意数组
 export type AnyArray = Array<any>
-export type AnyObjectOrArray = AnyObject | AnyArray
+// 任意 Map
+export type AnyMap = Map<any, any>
+// 任意 WeakMap
+export type AnyWeakMap = WeakMap<WeakKey, any>
+// 任意 WeakSet
+export type AnyWeakSet = WeakSet<WeakKey>
+// 任意 Set
+export type AnySet = Set<any>
+// 任意集合对象
+export type AnyCollection = AnyMap | AnyWeakMap | AnyWeakSet | AnySet
+// 任意对象类型 函数除外
+export type AnyObject = AnyRecord | AnyArray | AnyMap | AnySet | AnyWeakMap | AnyWeakSet
 
 // 深度只读
 export type DeepReadonly<T> = T extends object
