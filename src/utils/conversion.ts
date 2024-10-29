15d43c8698af1f7b72e413252e3a7a74a8871fb2
ZhuChongLin <8210856@qq.com>
2024-10-29 20:17:44 +0800

    refactor(utils): 优化 numStrToNum 函数的类型定义- 使用泛型扩展 numStrToNum 函数，使其能够根据输入字符串的类型返回相应的结果类型- 添加类型断言，确保函数返回值符合预期类型

--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -6,12 +6,15 @@
  *
  * @returns {number | string} 如果是纯数字字符串，则返回number；否则返回原字符串。
  */
-export function numStrToNum(str: string, radix: number = 10): number | string {
+export function numStrToNum<T extends string>(
+  str: T,
+  radix: number = 10
+): T extends `${number}` ? number : T {
   // 使用正则表达式检查字符串是否为整数
   const integerRegex = /\d+$/
   if (integerRegex.test(str)) {
     // 使用 parseInt 或一元加号将字符串转换为数字
-    return parseInt(str, radix)
+    return parseInt(str, radix) as any
   }
-  return str
+  return str as any
 }
