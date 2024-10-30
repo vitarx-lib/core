dc57b2a5e6586860ddf0d79fcd8b2d90a565bbb2
ZhuChongLin <8210856@qq.com>
2024-10-30 23:17:04 +0800

    refactor(core): 更新代理对象标识符
    - 移除 PROXY_DESTROYED_METHOD_SYMBOL 标识符
    - 添加 VALUE_PROXY_SYMBOL 标识符
    - 为 PROXY_SYMBOL、PROXY_DEEP_SYMBOL 和 VALUE_PROXY_SYMBOL 添加注释

--- src/core/variable/constants.ts
+++ src/core/variable/constants.ts
@@ -1,5 +1,6 @@
+// 代理标识符
 export const PROXY_SYMBOL = Symbol('VITARX_PROXY_SYMBOL')
 // 深度代理标识符
 export const PROXY_DEEP_SYMBOL = Symbol('VITARX_PROXY_DEEP_SYMBOL')
-// 销毁方法标识符，用于销毁代理对象
-export const PROXY_DESTROYED_METHOD_SYMBOL = Symbol('PROXY_DESTROYED_METHOD_SYMBOL')
+// 值代理对象标识符
+export const VALUE_PROXY_SYMBOL = Symbol('VITARX_VALUE_PROXY_SYMBOL')
