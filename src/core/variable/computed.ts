29eb3f85cf223ae345bd07c9dc9bb2bd5b34f303
ZhuChongLin <8210856@qq.com>
2024-10-30 18:57:23 +0800

    feat(core): 添加计算属性 Computed
    - 实现了 Computed 类，用于创建计算属性
    -计算属性可以监听多个依赖的变化，当依赖发生变化时，会重新运行计算函数并更新结果
    - 提供了 value 属性的 getter 和 setter 方法
    - 支持通过 computed 函数创建计算属性对象
    - 实现了依赖收集、结果缓存和监听器注册等功能

---
+++ src/core/variable/computed.ts
@@ -0,0 +1,136 @@
+import { ProxySymbol } from './helper.js'
+import { PROXY_DEEP_SYMBOL, PROXY_SYMBOL } from './constants'
+import { Depend } from './depend'
+import Listener from '../observer/listener'
+import Observers from '../observer/observers'
+
+/**
+ * # 计算属性
+ *
+ * 计算属性是依赖收集的一种实现，可以监听多个依赖的变化，当依赖发生变化时，会重新运行计算函数，并更新结果。
+ *
+ * 计算属性从本质上也是响应式对象，这也就意味着计算属性自身也能做为一个响应式对象依赖被引用或监听。
+ *
+ * 与`Ref`代理对象的使用方式几乎是一致的，但不同的是它的`value`一般是只读的，因为`value`是根据依赖变化而变化的。
+ */
+export class Computed<T> implements ProxySymbol {
+  readonly [PROXY_SYMBOL] = true
+  // 深度代理标识
+  readonly [PROXY_DEEP_SYMBOL] = false
+  #result: T
+  #initialize: boolean = false
+  readonly #getter
+  readonly #setter
+  // 监听器
+  #listener?: Listener
+
+  /**
+   * @param getter - 计算属性的getter函数
+   * @param setter - 计算属性的setter函数
+   */
+  constructor(getter: () => T, setter?: (newValue: T) => void) {
+    this.#getter = getter
+    this.#setter = setter
+    this.#result = undefined as T
+  }
+
+  /**
+   * 获取计算结果
+   *
+   * @returns - 计算结果
+   */
+  get value(): T {
+    if (!this.#initialize) this.#init()
+    Depend.track(this, 'value')
+    return this.#result
+  }
+
+  /**
+   * 修改计算结果
+   *
+   * 如果没有setter，则打印警告
+   *
+   * @param newValue
+   */
+  set value(newValue: T) {
+    if (this.#setter) {
+      this.#setter(newValue)
+    } else {
+      console.warn('不应该对计算属性进行写入，除非定义了setter。')
+    }
+  }
+
+  /**
+   * 转字符串方法
+   *
+   * @returns - 转换后的字符串
+   */
+  toString() {
+    if (this.#result?.toString) {
+      return this.#result.toString()
+    } else {
+      return `[Object Computed<${typeof this.#result}>]`
+    }
+  }
+
+  /**
+   * 停止计算属性
+   *
+   * 解除计算会停止对依赖的监听，并释放相关监听器。
+   */
+  stop() {
+    this.#listener?.destroy()
+  }
+
+  /**
+   * 初始化计算属性
+   *
+   * @private
+   */
+  #init() {
+    if (!this.#initialize) {
+      this.#initialize = true
+      const { result, deps } = Depend.collect(this.#getter)
+      this.#result = result
+      if (deps.size > 0) {
+        // 主监听器，用于依赖更新
+        this.#listener = Observers.register(deps, () => {
+          const newResult = this.#getter()
+          if (newResult !== this.#result) {
+            this.#result = newResult
+            Observers.trigger(this, 'value' as any)
+          }
+        })
+        // 依赖变更标记
+        const change = Symbol('computed depend change')
+        // 监听依赖的变化
+        const subListener = new Listener(function () {
+          Observers.trigger(deps, change as any)
+        })
+        deps.forEach((props, proxy) => {
+          Observers.registerProps(proxy, props, subListener)
+        })
+        // 销毁时，取消对依赖的监听
+        this.#listener.onDestroyed(() => {
+          subListener.destroy()
+          this.#listener = undefined
+        })
+      }
+    }
+  }
+}
+
+/**
+ * ## 创建一个计算属性
+ *
+ * 根据getter和setter创建一个计算属性，并返回一个计算属性对象。
+ *
+ * 计算属性的getter函数会在函数依赖发生变化时，自动执行，并返回新的计算结果。
+ *
+ * @template T - 结果值类型
+ * @param {() => T} getter - 计算属性的getter函数
+ * @param {(newValue: T) => void} setter - 计算属性的setter函数，只能修改计算属性的依赖值
+ */
+export function computed<T>(getter: () => T, setter?: (newValue: T) => void): Computed<T> {
+  return new Computed(getter, setter)
+}
