9709acbb98f3ee23611a63158cf94986b01f4dc7
ZhuChongLin <8210856@qq.com>
2024-10-31 13:45:09 +0800

    refactor(core): 重构 observer模块的导出方式
    - 移除原有的单独导出语句
    - 使用 export * 语法统一导出 listener.js 和 observers.js 的内容
    - 删除了多余的Observers 导出

--- src/core/observer/index.ts
+++ src/core/observer/index.ts
@@ -1,5 +1,3 @@
-import Listener from './listener.js'
-import Observers from './observers.js'
-
+export * from './listener.js'
+export * from './observers.js'
 export * from './helper.js'
-export { Listener, Observers }
