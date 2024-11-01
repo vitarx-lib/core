b78ed36fe5c4b5e2ab6fc91d69dd531ea7c3de74
ZhuChongLin <8210856@qq.com>
2024-11-01 14:37:44 +0800

    refactor(core): 重构 Dispose 类并实现 DisposeInterface 接口- 将 Dispose 类从 observer模块移动到 scope 模块 - 实现 DisposeInterface 接口，提供销毁、监听销毁、暂停和取消暂停功能 -优化 destroy 方法，清理所有回调函数释放内存 - 添加 isPaused属性和 pause、unpause 方法- 更新文件注释和类说明

---
+++ src/core/scope/dispose.ts
@@ -0,0 +1,97 @@
+import { AnyCallback, VoidCallback } from '../../types/common'
+
+/**
+ * 处置功能接口
+ *
+ * 实现 `DisposeInterface` 接口
+ */
+export interface DisposeInterface {
+  /** 销毁 */
+  destroy(): void
+
+  /** 监听销毁 */
+  onDestroyed(callback: AnyCallback): void
+
+  /** 暂停：可选 */
+  pause?(): void
+
+  /** 取消暂停：可选 */
+  unpause?(): void
+}
+
+/**
+ * # 可处置的对象
+ *
+ * 用于销毁/弃用对象，并释放内存。
+ */
+export class Dispose implements DisposeInterface {
+  // 销毁回调
+  #onDestroyedCallback?: VoidCallback[]
+  // 弃用状态
+  #isDeprecated = false
+  // 暂停状态
+  #pause = false
+
+  /**
+   * 是否已弃用/销毁
+   *
+   * @readonly
+   */
+  get isDeprecated() {
+    return this.#isDeprecated
+  }
+
+  /**
+   * 判断是否为暂停状态
+   */
+  get isPaused(): boolean {
+    return this.#pause
+  }
+
+  /**
+   * 销毁/弃用
+   *
+   * 调用此方法会将标记为弃用状态，并触发销毁回调，清理所有回调函数释放内存。
+   */
+  destroy(): void {
+    if (!this.#isDeprecated) {
+      this.#isDeprecated = true
+      if (this.#onDestroyedCallback) {
+        this.#onDestroyedCallback.forEach(callback => callback())
+        this.#onDestroyedCallback.length = 0
+        this.#onDestroyedCallback = undefined
+      }
+    }
+  }
+
+  /**
+   * 暂停
+   */
+  pause() {
+    this.#pause = true
+  }
+
+  /**
+   * 取消暂停
+   */
+  unpause() {
+    this.#pause = false
+  }
+
+  /**
+   * 监听销毁
+   *
+   * @param callback
+   */
+  onDestroyed(callback: VoidCallback): void {
+    if (this.#isDeprecated) {
+      callback()
+    } else {
+      if (this.#onDestroyedCallback) {
+        this.#onDestroyedCallback.push(callback)
+      } else {
+        this.#onDestroyedCallback = [callback]
+      }
+    }
+  }
+}
