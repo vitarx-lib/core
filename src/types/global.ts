515887bece516c735bbed93174a475be24da801e
ZhuChongLin <8210856@qq.com>
2024-11-04 19:18:11 +0800

    refactor(types): 为 global.ts 文件中的类型添加 JSDoc 注释
    - 为每个类型定义添加了 JSDoc 注释- 优化了代码格式，保持了一致性

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,37 +1,37 @@
 import { HtmlIntrinsicElements } from './html-elements'
 
 declare global {
-  // 任意函数
+  /** 任意函数 */
   type AnyFunction = (...args: any[]) => any
-  // 任意回调函数
+  /** 任意回调函数 */
   type AnyCallback = AnyFunction
-  // void 回调函数
+  /** void 回调函数 */
   type VoidCallback = VoidFunction
-  // 任意对象
+  /** 任意对象 */
   type AnyRecord = Record<any, any>
-  // 任意数组
+  /** 任意数组 */
   type AnyArray = Array<any>
-  // 任意 Map
+  /** 任意 Map */
   type AnyMap = Map<any, any>
-  // 任意 WeakMap
+  /** 任意 WeakMap */
   type AnyWeakMap = WeakMap<WeakKey, any>
-  // 任意 WeakSet
+  /** 任意 WeakSet */
   type AnyWeakSet = WeakSet<WeakKey>
-  // 任意 Set
+  /** 任意 Set */
   type AnySet = Set<any>
-  // 任意集合对象
+  /** 任意集合对象 */
   type AnyCollection = AnyMap | AnyWeakMap | AnyWeakSet | AnySet
-  // 任意对象类型 函数除外
+  /** 任意对象类型 函数除外 */
   type AnyObject = AnyRecord | AnyArray | AnyMap | AnySet | AnyWeakMap | AnyWeakSet
-  // 任意原始值类型
+  /** 任意原始值类型 */
   type AnyPrimitive = null | undefined | boolean | number | string | bigint | symbol
-  // 深度只读
+  /** 深度只读 */
   type DeepReadonly<T> = T extends object
     ? {
         readonly [P in keyof T]: DeepReadonly<T[P]>
       }
     : Readonly<T>
-  // 深度可写
+  /** 深度可写 */
   type unDeepReadonly<T> =
     T extends Readonly<object>
       ? {
