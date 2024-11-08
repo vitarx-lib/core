c6e37f19b6a8c53596a0770d034184595d1d6e18
ZhuChongLin <8210856@qq.com>
2024-11-09 04:05:30 +0800

    feat(core): 优化 FnWidget并添加新钩子
    -优化了 FnWidget 的导入语句，使用命名导入
    - 移除了不必要的空行
    - 修正了错误信息中的拼写错误- 添加了新的钩子函数 onBeforeUnmount，用于在组件销毁前执行回调

--- src/core/view/fn-widget.ts
+++ src/core/view/fn-widget.ts
@@ -1,4 +1,4 @@
-import type { IntrinsicAttributes, VNode } from './VNode.js'
+import { type IntrinsicAttributes, type VNode } from './VNode.js'
 import { Widget } from './widget.js'
 import { isFunction, isRecordObject } from '../../utils/index.js'
 import { LifeCycleHooks } from './life-cycle.js'
@@ -44,7 +44,6 @@ class FnWidgetProxy extends Widget {
       })
     }
   }
-
   /**
    * @inheritDoc
    *
@@ -181,6 +180,15 @@ export function onUpdated(fn: VoidCallback) {
 }
 
 /**
+ * 注册组件销毁前的钩子
+ *
+ * @param fn
+ */
+export function onBeforeUnmount(fn: VoidCallback) {
+  if (!isFunction(fn)) throw new TypeError(`无效的钩子函数，${typeof fn}`)
+  FnWidgetHookHandler.trackLifeCycle(LifeCycleHooks.beforeUnmount, fn)
+}
+/**
  * 注册组件渲染错误时的钩子
  *
  * @param fn
@@ -200,10 +208,10 @@ export function createFnWidget<P extends Record<string, any>>(
   fn: FnWidget<P>,
   props: P
 ): FnWidgetProxy {
-  const { build, exposed, lifeCycleHooks } = FnWidgetHookHandler.collect(fn, props)
+  let { build, exposed, lifeCycleHooks } = FnWidgetHookHandler.collect(fn, props)
   if (!isFunction(build)) {
     throw new Error(
-      `[Vitarx]：函数式小部件需要返回一个build函数创建响应式UI，实例：()=>Vitarx.VNode`
+      `[Vitarx]：函数式小部件需要返回一个build函数创建响应式UI，示例：()=>Vitarx.VNode`
     )
   }
   return new FnWidgetProxy(build, exposed, lifeCycleHooks)
