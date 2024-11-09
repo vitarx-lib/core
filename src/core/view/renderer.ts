0dd556262408e56106868685d6574b049aaac5df
ZhuChongLin <8210856@qq.com>
2024-11-10 01:52:47 +0800

    feat(core): 重构视图渲染器
    - 新增 WidgetRenderer 类用于管理小部件的渲染和更新
    - 优化了虚拟节点的创建和更新逻辑
    - 改进了属性和子节点的差异化更新算法
    - 重构了 Fragment节点的处理方式
    - 优化了事件处理和样式设置的代码

--- src/core/view/renderer.ts
+++ src/core/view/renderer.ts
@@ -1,11 +1,25 @@
-import { fragmentToNodes, type HtmlTagName, isRefEl, type VNode, type VNodeChild } from './VNode.js'
+import {
+  Fragment,
+  type FragmentTag,
+  type HtmlTagName,
+  isRefEl,
+  isVNode,
+  type VDocumentFragment,
+  type VElement,
+  type VNode,
+  type VNodeChild,
+  type VNodeChildren
+} from './VNode.js'
 import { isArray, isFunction, isRecordObject, isString } from '../../utils/index.js'
 import {
+  type ClassWidget,
   createScope,
   type HTMLClassProperties,
   type HtmlElementTags,
   type HTMLStyleProperties,
-  isClassWidget
+  isClassWidget,
+  reactive,
+  watchDepend
 } from '../../index.js'
 import { createFnWidget, type FnWidget } from './fn-widget.js'
 import type { Widget } from './widget.js'
@@ -16,40 +30,261 @@ import type { Widget } from './widget.js'
 export type ElementNode = Element | DocumentFragment
 
 /**
+ * 小部件元素管理器
+ */
+export class WidgetRenderer {
+  currentVNode: VNode
+
+  constructor(protected widget: Widget) {
+    const { result, listener } = watchDepend(this.build.bind(this), this.update.bind(this), {
+      getResult: true
+    })
+    if (!isVNode(result)) {
+      listener?.destroy()
+      throw new Error('[Vitarx]：Widget.build方法必须返回VNode虚拟节点')
+    }
+    this.currentVNode = result
+  }
+
+  /**
+   * 是否已挂载
+   */
+  get mounted(): boolean {
+    return this.currentVNode.el !== null
+  }
+
+  /**
+   * 获取节点对象
+   *
+   * @returns {VElement | null}
+   */
+  get el(): VElement | null {
+    return this.currentVNode.el
+  }
+
+  /**
+   * 获取父真实节点
+   */
+  get parentNode(): ParentNode | null {
+    return getParentNode(this.el)
+  }
+
+  /**
+   * 创建节点元素
+   *
+   * @returns {ElementNode}
+   */
+  createElement(): ElementNode {
+    return createElement(this.currentVNode)
+  }
+
+  /**
+   * 挂载节点
+   *
+   * @param parent
+   */
+  mount(parent: ElementNode): ElementNode {
+    let el: ElementNode
+    if (this.el) {
+      el = VElementToHTMLElement(this.el)
+    } else {
+      el = this.createElement()
+      this.widget.onMounted?.()
+    }
+    parent.append(el)
+    return el
+  }
+
+  build(): VNode {
+    try {
+      return this.widget.build()
+    } catch (e) {
+      if (this.widget?.onError && isFunction(this.widget.onError)) {
+        const vnode = this.widget.onError(e)
+        if (isVNode(vnode)) return vnode
+      }
+      // 继续向上抛出异常
+      throw e
+    }
+  }
+
+  update(force: boolean = false) {
+    const oldVNode = this.currentVNode
+    const newVNode = this.build()
+    this.currentVNode = this.patchUpdate(oldVNode, newVNode)
+  }
+
+  /**
+   * 差异更新
+   *
+   * @param oldVNode
+   * @param newVNode
+   */
+  protected patchUpdate(oldVNode: VNode, newVNode: VNode) {
+    // 类型不一致，替换原有节点
+    if (oldVNode.type !== newVNode.type || oldVNode.key !== newVNode.key) {
+      oldVNode.scope?.destroy()
+      const newEl = createElement(newVNode)
+      replaceChild(getParentNode(oldVNode.el)!, newEl as VElement, oldVNode.el!)
+      return newVNode
+    } else {
+      // 片段类型不具有任何属性
+      if (oldVNode.type !== Fragment) {
+        this.patchAttrs(oldVNode, newVNode)
+      }
+      // 更新子节点
+      if (!isFunction(oldVNode.type)) {
+        this.patchChildren(oldVNode, newVNode)
+      }
+      return oldVNode
+    }
+  }
+
+  /**
+   * 差异化更新属性
+   *
+   * @param oldVNode
+   * @param newVNode
+   */
+  protected patchAttrs(oldVNode: VNode, newVNode: VNode) {
+    const isWidget = isFunction(oldVNode.type), el = oldVNode.el as HTMLElement
+    const oldAttrs = oldVNode.props as Record<string, any>
+    const newAttrs = newVNode.props as Record<string, any>
+    // 使用 Set 记录 newAttrs 中的键
+    const newKeysSet = new Set(Object.keys(newAttrs))
+    // 遍历 oldAttrs，删除不在 newAttrs 中的键
+    Object.keys(oldAttrs).forEach(key => {
+      if (!newKeysSet.has(key)) {
+        if (isWidget) {
+          // @ts-ignore
+          delete oldAttrs[key]
+        } else {
+          if (key === 'className') {
+            el.className = ''
+          } else {
+            el.removeAttribute(key)
+          }
+        }
+      } else if (oldAttrs[key] !== newAttrs[key]) {
+        // 更新 oldAttrs 中值有变化的键
+        if (isWidget) {
+          oldAttrs[key] = newAttrs[key]
+        } else {
+          setAttr(el, key, newAttrs[key])
+        }
+      }
+    })
+    // 遍历 newAttrs，添加不在 oldAttrs 中的键
+    Object.keys(newAttrs).forEach(key => {
+      if (isWidget) {
+        if (!(key in oldAttrs)) {
+          if (isWidget) {
+            oldAttrs[key] = newAttrs[key]
+          } else {
+            setAttr(el, key, newAttrs[key])
+          }
+        }
+      }
+    })
+  }
+
+  /**
+   * 差异化更新子节点
+   *
+   * @param oldVNode
+   * @param newVNode
+   */
+  protected patchChildren(oldVNode: VNode, newVNode: VNode) {
+    const oldChildren = oldVNode.children
+    const newChildren = newVNode.children
+    if (oldChildren === newChildren) return
+    const isFragment = oldVNode.type === Fragment
+    // 如果没有旧的子节点
+    if (!oldChildren && newChildren) {
+      createChildren(VElementToHTMLElement(oldVNode.el!), newChildren)
+      oldVNode.children = newChildren
+      return
+    }
+    // 如果新子节点为空 则删除旧子节点
+    if (!newChildren && oldChildren) {
+      oldVNode.children = newChildren
+      return
+    }
+    // 处理字符串差异
+    // if (typeof oldChildren === 'string' && typeof newChildren === 'string') {
+    //   if (oldChildren !== newChildren) {
+    //     if (!isFragment) {
+    //       if (isArray(oldVNode.el)) {
+    //         oldVNode.el[0].nodeValue = newChildren
+    //       } else {
+    //         oldVNode.el!.firstChild!.nodeValue = newChildren
+    //       }
+    //       ;(oldVNode.props as any).children = newChildren
+    //     }
+    //   }
+    //   return
+    // }
+  }
+}
+
+/**
+ * 从Vnode el中获取父节点
+ *
+ * @param el
+ */
+function getParentNode(el: VElement | null): ParentNode | null {
+  if (!el) return null
+  return isArray(el) ? el[0].parentNode : el.parentNode
+}
+
+/**
+ * 替换节点
+ *
+ * @param parent
+ * @param newEl
+ * @param oldEl
+ */
+function replaceChild(parent: ParentNode, newEl: VElement | ElementNode, oldEl: VElement) {
+  if (isArray(oldEl)) {
+    const old = oldEl.shift()!
+    removeElement(oldEl)
+    parent.replaceChild(VElementToHTMLElement(newEl), old)
+  } else {
+    parent.replaceChild(VElementToHTMLElement(newEl), oldEl)
+  }
+}
+
+/**
+ * 删除元素
+ *
+ * @param el
+ */
+function removeElement(el: VElement) {
+  if (isArray(el)) {
+    // 删除旧节点
+    el.forEach(item => item.remove())
+  } else {
+    el.remove()
+  }
+}
+/**
  * 创建一个真实DOM元素
  *
  * @param vnode
- * @param container
  */
-export function createElement(vnode: VNode, container?: ElementNode): ElementNode {
+function createElement(vnode: VNode): ElementNode {
   let el: ElementNode
   switch (typeof vnode.type) {
     case 'string':
       // HTML 元素节点
       el = createHtmlElement(vnode as VNode<HtmlTagName>)
-      if (isRefEl(vnode.ref)) vnode.ref.value = el
       break
     case 'symbol':
       // Fragment 节点
-      el = document.createDocumentFragment()
-      updateChildren(el, vnode.children)
-      if (isRefEl(vnode.ref)) vnode.ref.value = fragmentToNodes(el)
+      el = createFragmentElement(vnode as VNode<FragmentTag>)
       break
     case 'function':
-      let component: Widget
-      const scope = createScope(() => {
-        const p = {
-          ...vnode.props,
-          children: vnode.children
-        }
-        // 函数组件或类组件
-        component = isClassWidget(vnode.type)
-          ? new vnode.type(p)
-          : createFnWidget(vnode.type as FnWidget, p)
-      })
-      if (isRefEl(vnode.ref)) vnode.ref.value = component!
-      vnode.scope = scope
-      el = component!.createElement().mount()
+      el = createWidgetElement(vnode as VNode<ClassWidget | FnWidget>)
       break
     default:
       throw new Error(`Unsupported vnode type: ${vnode.type}`)
@@ -59,41 +294,66 @@ export function createElement(vnode: VNode, container?: ElementNode): ElementNod
   } else {
     vnode.el = el
   }
-  if (container) container.appendChild(el)
-  return el
-}
-/**
- * 创建html元素
- *
- * @param vnode
- */
-function createHtmlElement(vnode: VNode<HtmlElementTags>) {
-  const el = document.createElement(vnode.type)
-  updateAttributes(el, vnode.props)
-  updateChildren(el, vnode.children)
   return el
 }
 
+// 创建小部件元素
+function createWidgetElement(vnode: VNode<FnWidget | ClassWidget>) {
+  let component: Widget
+  const scope = createScope(() => {
+    vnode.props = reactive(vnode.props, false)
+    // 函数组件或类组件
+    component = isClassWidget(vnode.type) ? new vnode.type(vnode.props) : createFnWidget(vnode.type as FnWidget, vnode.props)
+  })
+  if (isRefEl(vnode.ref)) vnode.ref.value = component!
+  vnode.scope = scope
+  return component!.renderer.mount()
+}
+
+// 创建html元素
+function createHtmlElement(vnode: VNode<HtmlElementTags>) {
+  const el = document.createElement(vnode.type)
+  setAttributes(el, vnode.props)
+  createChildren(el, vnode.children)
+  if (isRefEl(vnode.ref)) vnode.ref.value = el
+  return el
+}
+
+// 创建 Fragment 元素
+function createFragmentElement(vnode: VNode<FragmentTag>) {
+  const el = document.createDocumentFragment()
+  if (!vnode.children) {
+    // 创建一个空文本节点，用于占位 document.createComment('注释节点占位')
+    el.appendChild(document.createTextNode(''))
+    if (isRefEl(vnode.ref)) vnode.ref.value = []
+  } else {
+    createChildren(el, vnode.children)
+    if (isRefEl(vnode.ref)) vnode.ref.value = fragmentToNodes(el)
+  }
+  return el
+}
 /**
  * 更新DOM元素的子节点
  *
- * @param el
+ * @param parent
  * @param children
  */
-function updateChildren(el: ElementNode, children?: VNodeChild) {
+function createChildren(parent: ElementNode, children: VNodeChildren | undefined) {
   if (!children) return
-  if (typeof children === 'string') {
-    el.appendChild(document.createTextNode(children))
-  } else if (Array.isArray(children)) {
-    children.forEach(child => {
-      if (typeof child === 'string') {
-        el.appendChild(document.createTextNode(child))
-      } else {
-        createElement(child, el)
-      }
-    })
-  } else {
-    createElement(children, el)
+  children.forEach(child => createChild(parent, child))
+}
+
+/**
+ * 创建子节点
+ *
+ * @param parent
+ * @param child
+ */
+function createChild(parent: ElementNode, child: VNodeChild) {
+  if (isVNode(child)) {
+    parent.appendChild(createElement(child))
+  } else if (child?.toString && isFunction(child?.toString)) {
+    parent.appendChild(document.createTextNode(child.toString()))
   }
 }
 
@@ -102,42 +362,50 @@ function updateChildren(el: ElementNode, children?: VNodeChild) {
  *
  * @param el - 目标元素
  * @param props - 属性对象
- * @param exclude - 排除的属性
  */
-function updateAttributes(el: HTMLElement, props: Record<string, any>, exclude: string[] = []) {
-  for (const key in props) {
-    if (exclude.includes(key)) {
-      continue
-    }
-    const value = props[key]
-    switch (key) {
-      case 'style':
-        setStyle(el, value)
-        break
-      case 'class':
-        setClass(el, value)
-        break
-      default:
-        if (isHTMLNodeEvent(el, key)) {
-          if (!isFunction(value)) {
-            throw new TypeError(`无效的事件处理程序，${key}: ${typeof value}`)
-          }
-          el.addEventListener(key.slice(2).toLowerCase(), value)
-        } else {
-          try {
-            // 处理其他属性
-            if (key in el) {
-              // @ts-ignore
-              el[key] = value
-            } else {
-              el.setAttribute(key, value) // 设置未知属性
-            }
-          } catch (error) {
-            console.error(`设置属性 ${key} 时发生错误:`, error)
-          }
+function setAttributes(el: HTMLElement, props: Record<string, any>) {
+  Object.keys(props).forEach(key => {
+    if (key === 'children') return
+    setAttr(el, key, props[key])
+  })
+}
+
+// 设置Html元素属性
+function setAttr(el: HTMLElement, key: string, value: any, oldValue?: any) {
+  switch (key) {
+    case 'style':
+      setStyle(el, value)
+      break
+    case 'class':
+      setClass(el, value)
+      break
+    default:
+      if (isHTMLNodeEvent(el, key)) {
+        if (!isFunction(value)) {
+          throw new TypeError(`无效的事件处理程序，${key}: ${typeof value}`)
         }
-        break
-    }
+        const event = key.slice(2).toLowerCase()
+        // 删除旧的事件
+        if (oldValue && isFunction(oldValue)) {
+          el.removeEventListener(event, oldValue)
+        }
+        el.addEventListener(event, value)
+      } else if (key.startsWith('data-')) {
+        el.dataset[key.slice(5)] = value
+      } else {
+        try {
+          // 处理其他属性
+          if (key in el) {
+            // @ts-ignore
+            el[key] = value
+          } else {
+            el.setAttribute(key, value) // 设置未知属性
+          }
+        } catch (error) {
+          console.error(`设置属性 ${key} 时发生错误:`, error)
+        }
+      }
+      break
   }
 }
 
@@ -158,7 +426,7 @@ function isHTMLNodeEvent(el: HTMLElement, prop: string) {
  * @param style
  */
 function setStyle(el: HTMLElement, style: HTMLStyleProperties) {
-  if (style) {
+  if (style && el.style) {
     if (isString(style)) {
       el.style.cssText = style
     } else if (isRecordObject(style)) {
@@ -192,4 +460,40 @@ function setClass(el: HTMLElement, classData: HTMLClassProperties) {
       }
     }
   }
+}
+
+/**
+ * node数组转换为片段
+ *
+ * @param nodes
+ */
+function nodesToFragment(nodes: VDocumentFragment): DocumentFragment {
+  const el = document.createDocumentFragment()
+  for (let i = 0; i < nodes.length; i++) {
+    el.appendChild(nodes[i])
+  }
+  return el
+}
+
+/**
+ * 片段转node数组
+ *
+ * @param el
+ */
+function fragmentToNodes(el: DocumentFragment): VDocumentFragment {
+  const els: Node[] = []
+  for (let i = 0; i < el.childNodes.length; i++) {
+    els.push(el.childNodes[i])
+  }
+  return els as VDocumentFragment
+}
+
+/**
+ * VElement 转 HTMLElement
+ *
+ * @param el
+ * @constructor
+ */
+function VElementToHTMLElement(el: VElement | ElementNode): ElementNode {
+  return isArray(el) ? nodesToFragment(el) : el
 }
