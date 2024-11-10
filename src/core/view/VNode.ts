e1cacd15db98de5bb7fb62fb7ee1e0962ed5a494
ZhuChongLin <8210856@qq.com>
2024-11-10 19:42:02 +0800

    refactor(core): 重构虚拟节点并添加文本节点支持
    - 添加 TextVNode 类型和相关处理逻辑
    - 重构 VNode 类型定义，支持 Fragment 和 Text 类型
    - 修改 createVNode函数以支持文本节点
    - 添加 isTextVNode 函数用于检测文本节点

--- src/core/view/VNode.ts
+++ src/core/view/VNode.ts
@@ -2,24 +2,37 @@ import {
   type HtmlElementTagMap,
   type HtmlElementTags,
   type HtmlIntrinsicElements,
+  isArray,
+  isFunction,
   isRecordObject,
   popProperty,
-  Scope
+  Scope,
+  type WidgetChildren
 } from '../../index.js'
 import { type ClassWidget } from './widget.js'
 import type { FnWidget } from './fn-widget.js'
 
+/** 响应式元素引用标记 */
+const RefElSymbol = Symbol('RefEl')
+/** 文本节点标识符 */
+const TextVNodeSymbol = Symbol('TextNode')
 /** 片段组件标识符 */
 export const Fragment: unique symbol = Symbol('Fragment')
-const RefElSymbol = Symbol('RefEl')
 /** 片段类型标识符 */
-export type FragmentTag = typeof Fragment
+export type Fragment = typeof Fragment
 /** 虚拟节点标识符 */
 const VNodeSymbol = Symbol('VNode')
+
+/** 文本节点 */
+export interface TextVNode {
+  value: string
+  el: Text | null
+  [TextVNodeSymbol]: true
+}
 /** 唯一标识符 */
 export type OnlyKey = string | number | bigint
-// 计算出元素类型
-type ComputedRefElType<T> = T extends HtmlElementTags ? HtmlElementTagMap[T] : T extends FragmentTag ? VDocumentFragment : T extends FnWidget ? Record<string, any> : T
+// 辅助计算出元素类型
+type ComputedRefElType<T> = T extends HtmlElementTags ? HtmlElementTagMap[T] : T extends Fragment ? VDocumentFragment : T extends FnWidget ? Record<string, any> : T
 /** 引用元素类型 */
 export type RefEl<T> = {
   value: ComputedRefElType<T> | null, readonly [RefElSymbol]: true
@@ -49,7 +62,7 @@ export interface IntrinsicAttributes {
  */
 export type HtmlTagName = HtmlElementTags
 // 节点类型
-export type VNodeType = HtmlTagName | FragmentTag | ClassWidget | FnWidget
+export type VNodeType = HtmlTagName | Fragment | Text | ClassWidget | FnWidget
 // 节点属性结构
 export type VNodeProps<T> = (T extends HtmlElementTags
   ? HtmlIntrinsicElements[T]
@@ -59,28 +72,35 @@ export type VNodeProps<T> = (T extends HtmlElementTags
       ? P
       : {}) &
   IntrinsicAttributes
-
+// 辅助计算出Children类型
+type ComputedVNodeChildren<T> = T extends HtmlElementTags ? VNodeChild | VNodeChild[] : T extends Fragment ? VNodeChild | VNodeChild[] : WidgetChildren<VNodeProps<T>>
 /** 子节点类型 */
-export type VNodeChild = string | VNode | Array<VNode | string>
+export type VNodeChild = VNode | string | TextVNode
+/** 子节点列表 */
+export type VNodeChildren = Array<VNodeChild>
 /** HTML片段节点数组 */
 export type VDocumentFragment = Array<Element | Text>
 /** 真实元素对象，片段节点为数组 */
-export type VElement = Element | VDocumentFragment
+export type VElement = Element | Text | VDocumentFragment
 
 /**
  * 虚拟Node
  *
  * - `type`: 节点类型
  * - `props`: 节点属性
+ * - `key`: 节点唯一标识
+ * - `ref`: 节点引用
+ * - `scope`: 作用域，内部创建，用于管理副作用
+ * - `children`: 子节点列表，如果是函数小部件或类小部件，则此参数会覆盖到`props.children`
  */
 export interface VNode<T extends VNodeType = VNodeType> {
   type: T
   props: VNodeProps<T>
-  children: VNodeChild | undefined
   key: OnlyKey | null
   ref: RefEl<any> | null
   el: VElement | null
   scope: Scope | null
+  children: VNodeChildren | undefined
   [VNodeSymbol]: true
 }
 /**
@@ -88,24 +108,53 @@ export interface VNode<T extends VNodeType = VNodeType> {
  *
  * @param type - 节点类型
  * @param props - 节点属性
+ * @param children - 子节点，如果是小部件类型则会写入到 `props.children`
+ * @returns {VNode} - 虚拟节点
  */
 export function createVNode<T extends VNodeType>(
   type: T,
-  props: IntrinsicAttributes & VNodeProps<T>
+  props: IntrinsicAttributes & VNodeProps<T>,
+  children?: ComputedVNodeChildren<T>
 ): VNode<T> {
   if (!isRecordObject(props)) {
     throw new TypeError(
       `[Vitarx]：createVNode.props参数类型必须是一个键值对对象，给定${typeof props}`
     )
   }
+  // 如果不是函数，则将children转为数组，方便后续处理
+  if (isFunction(type)) {
+    if (children !== undefined) {
+      // @ts-ignore
+      props.children = children
+      children = undefined
+    }
+  } else {
+    const pc = popProperty(props as any, 'children')
+    children = children || pc
+    if (children) {
+      if (!isArray(children)) {
+        // @ts-ignore
+        children = [children]
+      }
+      children.forEach((child, i) => {
+        if (!isVNode(child)) {
+          // @ts-ignore
+          children[i] = {
+            value: String(child),
+            el: null,
+            [TextVNodeSymbol]: true
+          }
+        }
+      })
+    }
+  }
   const key = popProperty(props, 'key') || null
   const ref = popProperty(props, 'ref') || null
-  const children = popProperty(props as any, 'children')
   return {
     [VNodeSymbol]: true,
     type,
     props,
-    children,
+    children: children as VNodeChildren,
     key,
     ref,
     el: null,
@@ -122,6 +171,14 @@ export function isVNode(obj: any): obj is VNode {
   return obj?.[VNodeSymbol] === true
 }
 
+/**
+ * 判断是否为文本节点
+ *
+ * @param obj
+ */
+export function isTextVNode(obj: any): obj is TextVNode {
+  return obj?.[TextVNodeSymbol] === true
+}
 /**
  * 引用元素
  *
@@ -142,3 +199,4 @@ export function refEl<T>(): RefEl<T> {
 export function isRefEl(obj: any): obj is RefEl<any> {
   return obj?.[RefElSymbol] === true
 }
+
