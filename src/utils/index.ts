447a37fd8da4dda0e00b9bf393619b98ae7482c2
ZhuChongLin <8210856@qq.com>
2024-11-13 19:33:55 +0800

    refactor(utils): 添加常用类型定义
    - 在 utils/index.ts 中添加了多个常用类型定义
    -包括 AnyFunction、AnyCallback、VoidCallback、AnyRecord、AnyArray 等
    - 新增 DeepReadonly 和 unDeepReadonly 类型用于深度只读和可写转换
    - 添加 MakeRequired 类型用于将接口的部分属性变为必填项

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,47 @@ export * from './diff.js'
 export * from './conversion.js'
 export * from './uuid.js'
 export * from './quick.js'
+
+declare global {
+  /** 任意函数 */
+  type AnyFunction = (...args: any[]) => any
+  /** 任意回调函数 */
+  type AnyCallback = AnyFunction
+  /** void 回调函数 */
+  type VoidCallback = VoidFunction
+  /** 任意对象 */
+  type AnyRecord = Record<any, any>
+  /** 任意数组 */
+  type AnyArray = Array<any>
+  /** 任意 Map */
+  type AnyMap = Map<any, any>
+  /** 任意 WeakMap */
+  type AnyWeakMap = WeakMap<WeakKey, any>
+  /** 任意 WeakSet */
+  type AnyWeakSet = WeakSet<WeakKey>
+  /** 任意 Set */
+  type AnySet = Set<any>
+  /** 任意集合对象 */
+  type AnyCollection = AnyMap | AnyWeakMap | AnyWeakSet | AnySet
+  /** 任意对象类型 函数除外 */
+  type AnyObject = AnyRecord | AnyArray | AnyMap | AnySet | AnyWeakMap | AnyWeakSet
+  /** 任意原始值类型 */
+  type AnyPrimitive = null | undefined | boolean | number | string | bigint | symbol
+  /** 深度只读 */
+  type DeepReadonly<T> = T extends object
+    ? {
+        readonly [P in keyof T]: DeepReadonly<T[P]>
+      }
+    : Readonly<T>
+  /** 深度可写 */
+  type unDeepReadonly<T> =
+    T extends Readonly<object>
+      ? {
+          [P in keyof T]: unDeepReadonly<T[P]>
+        }
+      : T
+  /** 让接口的部分属性 为必填项 */
+  type MakeRequired<T extends object, K extends keyof T> = T & {
+    [P in K]-?: T[P] // 强制指定的属性 K 为必填
+  }
+}
