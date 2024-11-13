b3aa1e259cb801285ba983c99770c2952964acc7
ZhuChongLin <8210856@qq.com>
2024-11-13 15:40:05 +0800

    feat(variable): 优化依赖收集机制
    - 新增 #collector 属性，用于存储当前收集器
    - 在 track 方法中增加对当前收集器的处理逻辑，提高收集效率
    - 重构 collect 和 get 方法，支持 'self' 和 'all' 两种作用域
    -优化 get 方法的实现，使用新的收集器进行依赖收集

--- src/core/variable/depend.ts
+++ src/core/variable/depend.ts
@@ -22,7 +22,7 @@ type Result<T> = {
 export class Depend {
   // 收集器集合
   static #collectors = new Map<symbol, Deps>()
-
+  static #collector: undefined | Deps
   /**
    * ## 跟踪依赖，记录引用
    *
@@ -35,6 +35,16 @@ export class Depend {
    */
   static track(proxy: AnyProxy, prop: PropName): void {
     if (!isProxy(proxy)) return
+    if (this.#collector) {
+      if (this.#collector.has(proxy)) {
+        // 如果已经添加了该代理对象的依赖，则只添加键
+        this.#collector.get(proxy)!.add(prop)
+      } else {
+        // 否则添加代理对象和键
+        const keys: Set<PropName> = new Set([prop])
+        this.#collector.set(proxy, keys)
+      }
+    }
     if (this.#collectors.size) {
       // 遍历收集器，并记录引用
       this.#collectors.forEach(collector => {
@@ -58,10 +68,12 @@ export class Depend {
    * > **注意**：函数执行过程中，会自动收集依赖，无需手动调用{@link track}方法。
    *
    * @alias get
-   * @param {Function} fn 任意可执行的函数。
+   * @param fn 任意可执行的函数。
+   * @param scope - 作用域，如果为all时，则会收集所有依赖，包括计算属性中的依赖。
    * @returns { Deps } `Map对象`，键为依赖的根代理对象，值为引用的键索引`Set对象`，存在`.`连接符代表多层引用
    */
-  static collect<T>(fn: () => T): Result<T> {
+  static collect<T>(fn: () => T, scope: 'self' | 'all' = 'all'): Result<T> {
+    if (scope === 'self') return this.get(fn, 'self')
     // 创建临时依赖id
     const id = Symbol('id')
     // 创建依赖集合
@@ -84,8 +96,19 @@ export class Depend {
    * @alias collect
    * @see collect
    */
-  static get<T>(fn: () => T): Result<T> {
-    return this.collect(fn)
+  static get<T>(fn: () => T, scope: 'self' | 'all' = 'self'): Result<T> {
+    if (scope === 'all') return this.collect(fn, 'all')
+    const oldCollector = this.#collector
+    // 创建依赖集合
+    this.#collector = new Map()
+    try {
+      const result = fn()
+      // 返回依赖集合
+      return { result, deps: this.#collector } as any
+    } finally {
+      // 删除收集器
+      this.#collector = oldCollector
+    }
   }
 }
 
