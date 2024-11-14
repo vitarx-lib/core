67899377325c8e0036aea8fa8dbda7906c9f363c
ZhuChongLin <8210856@qq.com>
2024-11-15 03:08:38 +0800

    refactor(core): 重构 Scope作用域类
    - 将 DisposeEffect 重命名为 Effect，简化继承结构
    - 优化 Scope 类构造函数，增加 toParent 参数
    - 修改 AutoDisposed 类，使其自动添加到父级作用域
    - 更新相关方法名称，如 addDispose改为 addEffect
    -调整内部逻辑，提高代码可读性和维护性

--- src/core/scope/scope.ts
+++ src/core/scope/scope.ts
@@ -1,37 +1,38 @@
-import { DisposeEffect, DisposeInterface } from './dispose-effect.js'
+import { Effect, type EffectInterface, isEffect } from './effect.js'
 
 /**
  * # 自动处置
  *
- * 当类实例化时会自动将实例对象添加到当前 {@link Scope} 作用域中，当作用域销毁时，会自动触发该实例的销毁方法，从而实现自动清理。
+ * 当类实例化时会自动将实例对象添加到父级 {@link Scope} 作用域中进行管理，当父级作用域销毁时，会自动触发该实例的销毁方法，从而实现自动清理。
  */
-export class AutoDisposed extends DisposeEffect {
+export class AutoDisposed extends Effect {
   constructor() {
     super()
-    // 添加到当前作用域中进行管理
-    getCurrentScope()?.add(this)
+    // 添加到父级作用域中进行管理
+    addEffect(this)
   }
 }
 
 /**
  * # 作用域管理器
  */
-export class Scope extends DisposeEffect {
+export class Scope extends Effect {
   /** 临时记录当前作用域 */
   static #currentScope?: Scope
   /** 副作用 */
-  #effects?: Set<DisposeInterface> = new Set()
+  #effects?: Set<EffectInterface> = new Set()
 
   /**
    * 实例化一个作用域管理器
    *
-   * @param main
+   * @param main - 主函数
+   * @param toParent - 是否添加到父级作用域中，如果有。
    */
-  constructor(main: () => void) {
+  constructor(main: () => void, toParent: boolean = true) {
     super()
     const oldScope = Scope.#currentScope
     // 添加到父级作用域中
-    oldScope?.add(this)
+    if (toParent) oldScope?.add(this)
     Scope.#currentScope = this
     main()
     Scope.#currentScope = oldScope
@@ -58,16 +59,17 @@ export class Scope extends DisposeEffect {
   /**
    * 添加一个可处置的副作用到当前作用域中。
    *
-   * 如果习惯类编程则可以继承自{@link DisposeEffect} 或 {@link AutoDisposed} 类；
+   * 如果习惯类编程则可以继承自{@link Effect} 或 {@link AutoDisposed} 类；
    * 函数式编程则可以传入一个对象，对象必须具有`destroy`和`onDestroyed`属性方法。
    *
-   * @param dispose - 处置对象
+   * @param effect - 处置对象
    * @returns {boolean} - 是否添加成功
    */
-  add(dispose: DisposeInterface): boolean {
+  add(effect: EffectInterface): boolean {
     if (!this.isDeprecated) {
-      this.#effects?.add(dispose)
-      dispose.onDestroyed(() => this.#effects?.delete(dispose))
+      isEffect(effect, true)
+      this.#effects?.add(effect)
+      effect.onDestroyed(() => this.#effects?.delete(effect))
       return true
     } else {
       console.trace('当前作用域已被销毁，不应该再往作用域中增加可处置的副作用对象')
@@ -117,10 +119,11 @@ export class Scope extends DisposeEffect {
  * ## 创建作用域
  *
  * @param main 入口函数
+ * @param toParent - 是否添加到父级作用域中，默认为true
  * @returns {Scope} 返回作用域实例，提供了`destroy`方法来销毁作用域
  */
-export function createScope(main: () => void): Scope {
-  return new Scope(main)
+export function createScope(main: () => void, toParent: boolean = true): Scope {
+  return new Scope(main, toParent)
 }
 
 /**
@@ -135,9 +138,9 @@ export function getCurrentScope(): Scope | undefined {
 /**
  * ## 往作用域中添加一个可处置对象
  *
- * @param dispose - 可处置对象
  * @returns {boolean} - 是否添加成功
+ * @param effect
  */
-export function addDispose(dispose: DisposeInterface): boolean {
-  return !!getCurrentScope()?.add(dispose)
+export function addEffect(effect: EffectInterface): boolean {
+  return !!getCurrentScope()?.add(effect)
 }
