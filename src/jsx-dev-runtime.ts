986e95a4c019693c5ba01f19d234e7f3f2d6d793
ZhuChongLin <8210856@qq.com>
2024-11-18 17:33:11 +0800

    refactor(dev): 重构 jsxDEV 函数以支持 HMR
    - 增加 DevVNode接口以扩展 VNode 结构
    - 实现热模块替换（HMR）功能
    - 优化开发模式下的 vnode 创建过程
    - 添加类型注释和参数重命名以提高代码可读性

--- src/jsx-dev-runtime.ts
+++ src/jsx-dev-runtime.ts
@@ -3,25 +3,46 @@ import { jsx } from './jsx-runtime.js'
 
 type Source = { fileName: string; lineNumber: number; columnNumber: number }
 
+interface DevVNode<T extends VNodeType> extends VNode<T> {
+  devInfo: {
+    source: Source
+    isStatic: boolean
+    self: any
+  }
+}
 /**
  * JSX构建VNode(开发模式)
  *
- * @param type
- * @param props
- * @param key
- * @param _isStatic
- * @param _source
- * @param _self
+ * @param type - 类型
+ * @param props - 属性
+ * @param key - 唯一标识
+ * @param isStatic - 是否静态
+ * @param source - 源码位置信息
+ * @param self - 绑定的this
  */
 function jsxDEV<T extends VNodeType>(
   type: T,
   props: VNodeProps<T> | null,
   key: Vitarx.GlobalIntrinsicAttributes['key'] | undefined,
-  _isStatic: boolean,
-  _source: Source,
-  _self: any
-): VNode<T> {
-  return jsx(type, props, key)
+  isStatic: boolean,
+  source: Source,
+  self: any
+): DevVNode<T> {
+  // @ts-ignore 开发模式获取新模块
+  if (import.meta.env?.MODE === 'development') {
+    if (typeof type === 'function') {
+      // @ts-ignore
+      const newModule = window['__$vitarx_widget_hmr_map$__']?.get?.(type)
+      if (newModule) type = newModule
+    }
+  }
+  const vnode = jsx(type, props, key) as DevVNode<T>
+  vnode.devInfo = {
+    source,
+    isStatic,
+    self
+  }
+  return vnode
 }
 
 export { Fragment, jsxDEV }
