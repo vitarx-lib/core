2a7d2f0e048f7bce1135b8273efac87a47160ecc
ZhuChongLin <8210856@qq.com>
2024-11-30 11:52:51 +0800

    refactor: 简化 JSX Runtime 类型定义
    - 移除了不必要的类型声明和导出- 通过从 '../dist/jsx-runtime.js' 导出所有内容，简化了代码结构

--- jsx-runtime/index.d.ts
+++ jsx-runtime/index.d.ts
@@ -1,14 +1,1 @@
-import { Fragment, type VNode, type VNodeProps, type VNodeType } from './../dist/index.js'
-
-/**
- * JSX构建VNode
- *
- * @see createElement
- */
-declare function jsx<T extends VNodeType>(
-  type: T,
-  props?: VNodeProps<T> | null,
-  key?: Vitarx.GlobalIntrinsicAttributes['key']
-): VNode<T>
-
-export { Fragment, jsx, jsx as jsxs }
+export * from '../dist/jsx-runtime.js'
