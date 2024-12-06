89a21ee30ddd90e63ec490476a3f23fa8809af7b
ZhuChongLin <8210856@qq.com>
2024-12-06 19:57:39 +0800

    feat(core): 添加内置组件导出索引文件
    - 新增 index.ts 文件，统一导出内置组件
    - 导出 LazyWidget、Suspense 和 AsyncWidget 组件
    - 提供 getSuspenseCounter函数导出

---
+++ src/core/widget/built-in/index.ts
@@ -0,0 +1,3 @@
+export { default as LazyWidget } from './LazyWidget.js'
+export { default as Suspense, getSuspenseCounter } from './Suspense.js'
+export { default as AsyncWidget } from './AsyncWidget.js'
