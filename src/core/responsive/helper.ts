39ce21dd35b4cbf67a65261145b50fbe9fb241ec
ZhuChongLin <8210856@qq.com>
2024-12-22 12:52:28 +0800

    feat(core): 添加 markRaw 和 isMarkRaw 方法
    - 新增 markRaw 方法，用于标记对象不被代理
    - 新增 isMarkRaw 方法，用于判断对象是否被标记为不代理
    - 添加 isMakeProxy 方法，用于判断对象是否需要被代理
    - 更新导入信息，引入 UN_PROXY_SYMBOL 和 isObject

--- src/core/responsive/helper.ts
+++ src/core/responsive/helper.ts
@@ -1,7 +1,13 @@
 // 代理标识符
 import { type Ref } from './ref.js'
 import type { Reactive, ReactiveSymbol, UnReactive } from './reactive.js'
-import { PROXY_DEEP_SYMBOL, PROXY_SYMBOL, VALUE_PROXY_SYMBOL } from './constants.js'
+import {
+  PROXY_DEEP_SYMBOL,
+  PROXY_SYMBOL,
+  UN_PROXY_SYMBOL,
+  VALUE_PROXY_SYMBOL
+} from './constants.js'
+import { isObject } from '../../utils/index.js'
 
 /**
  * 代理标识接口
@@ -68,4 +74,39 @@ export function isDeepProxy(proxy: ProxySymbol): boolean {
  */
 export function isValueProxy(val: any): val is ValueProxy<any> {
   return val?.[VALUE_PROXY_SYMBOL] === true
+}
+
+/**
+ * 标记为不可代理对象
+ *
+ * 它与`shallowReactive`有异曲同工之妙，`shallowReactive`是不代理所有子对象，
+ * 而`markRaw`则是标记某个子对象不被代理，通常这个标记为不被代理的对象是不可变的。
+ *
+ * @param {AnyObject} obj - 任意对象！
+ */
+export function markRaw<T extends AnyObject>(obj: T): T {
+  Object.defineProperty(obj, UN_PROXY_SYMBOL, {
+    value: true
+  })
+  return obj
+}
+
+/**
+ * 判断是否为不可代理对象
+ *
+ * @param obj
+ * @returns {boolean} - true为是不可代理对象，false则不是
+ */
+export function isMarkRaw(obj: AnyObject): boolean {
+  return !!(obj as any)[UN_PROXY_SYMBOL]
+}
+
+/**
+ * 判断是否需要代理对象
+ *
+ * @param {any} value
+ * @internal 内部使用的助手函数！
+ */
+export function isMakeProxy(value: any): value is object {
+  return isObject(value) && !isProxy(value) && !isMarkRaw(value)
 }
