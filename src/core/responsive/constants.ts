cf56341656ede6a6c0d04bdf7cfd079cf9c643e4
ZhuChongLin <8210856@qq.com>
2024-12-22 12:52:37 +0800

    feat(core): 添加 UN_PROXY_SYMBOL常量
    - 在 responsive模块的 constants.ts 文件中添加了 UN_PROXY_SYMBOL 常量
    - 该常量用于标识不可代理的对象

--- src/core/responsive/constants.ts
+++ src/core/responsive/constants.ts
@@ -4,3 +4,5 @@ export const PROXY_SYMBOL = Symbol('VITARX_PROXY_SYMBOL')
 export const PROXY_DEEP_SYMBOL = Symbol('VITARX_PROXY_DEEP_SYMBOL')
 // 值代理对象标识符
 export const VALUE_PROXY_SYMBOL = Symbol('VITARX_VALUE_PROXY_SYMBOL')
+// 不可代理标识
+export const UN_PROXY_SYMBOL = Symbol('VITARX_UN_PROXY_SYMBOL')
