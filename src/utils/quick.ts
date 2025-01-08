7427a290903888f0c7873dcebeb1cd6b964dc56f
ZhuChongLin <8210856@qq.com>
2025-01-08 19:04:45 +0800

    feat(utils): 深度合并对象函数增加新功能并优化逻辑
    - 添加 allowMergeUndefined 参数，控制是否合并值为 undefined 的属性
    - 增加 Object.is 判断，避免不必要的合并操作
    - 优化源对象属性为 undefined 时的处理逻辑

--- src/utils/quick.ts
+++ src/utils/quick.ts
@@ -33,22 +33,31 @@ export function sleep(time: number): Promise<unknown> {
 /**
  * 合并对象
  *
- * 注意：将源对象中的属性覆盖目标对象中的属性，如果属性值是对象则会深度合并。
+ * 将源对象中的属性覆盖目标对象中的属性，如果属性值是对象则会深度合并。
  *
  * @param {object} target - 目标对象
  * @param {object} source - 源对象
+ * @param {boolean} [allowMergeUndefined=false] - 是否允许合并值为undefined的属性到目标对象中，默认不允许
  * @returns {object} 合并后的对象
  */
-export function deepMergeObject<R = any>(target: AnyRecord, source: AnyRecord): R {
+export function deepMergeObject<R = any>(
+  target: AnyRecord,
+  source: AnyRecord,
+  allowMergeUndefined = false
+): R {
+  if (Object.is(target, source)) return target
   for (const key in source) {
     if (source.hasOwnProperty(key)) {
       if (typeof source[key] === 'object' && source[key] !== null && !Array.isArray(source[key])) {
         if (!target[key]) Object.assign(target, { [key]: {} })
         deepMergeObject(target[key], source[key])
       } else {
-        target[key] = source[key]
+        if (source[key] !== undefined || allowMergeUndefined) {
+          target[key] = source[key]
+        }
       }
     }
   }
   return target
 }
+
