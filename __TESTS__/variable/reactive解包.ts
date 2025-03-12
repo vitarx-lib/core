558c1db37c11f32314d6c271c7d64a5abd7e6983
ZhuChongLin <8210856@qq.com>
2025-03-13 00:24:46 +0800

    test(reactive): 更新 reactive 解包测试用例
    - 添加 isValueProxy、shallowReadonly导入
    - 优化测试用例，增加对不同数据结构嵌套 ref 的解包测试
    - 修改输出信息，更直观地展示解包结果

--- __TESTS__/variable/reactive解包.ts
+++ __TESTS__/variable/reactive解包.ts
@@ -1,26 +1,21 @@
-import { reactive, ref } from '../../src/index.js'
+import { isValueProxy, reactive, ref, shallowReadonly } from '../../src/index.js'
 
 const books = reactive([ref(0)])
-// 不会解构
-console.log(books[0])
-
-books[0] = 1
-console.log(books[0])
+// 自动解构
+console.log('数组嵌套ref是否解构,', !isValueProxy(books[0]))
 
 const map = reactive(new Map([['count', ref(0)]]))
 // 这里需要 .value
-console.log(map.get('count')!.value)
+console.log('map嵌套ref是否解构', !isValueProxy(map.get('count')!))
 
-const set = reactive(new Set([[ref(0)]]))
+const set = reactive(new Set([ref(0)]))
+
 // 不会解构
-console.log(set.values())
+console.log('set嵌套ref是否自动解构', !isValueProxy(set.values().next().value))
 
-const data = reactive(
-  {
-    count: ref(0)
-  },
-  false
-)
+const data = shallowReadonly({
+  count: ref(0)
+})
 
 // 浅层代理不会解构
-console.log(data.count)
+console.log('浅层代理嵌套ref是否解构', !isValueProxy(data.count))
