e7e3627e59639d6ab5179546fda920dc44ad503b
ZhuChongLin <8210856@qq.com>
2025-03-29 02:04:58 +0800

    refactor(responsive): 更新 Context导入路径
    - 将 Context 导入路径从 '../../src/context/context' 修改为 '../../src/context/index'
    - 这个改动可能是为了统一导出路径，提高代码的可维护性

--- packages/responsive/__tests__/context/context.test.ts
+++ packages/responsive/__tests__/context/context.test.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, it } from 'vitest'
-import { Context } from '../../src/context/context'
+import { Context } from '../../src/context/index'
 
 describe('Context', () => {
   beforeEach(() => {
