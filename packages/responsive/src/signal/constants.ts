41003f7a5ae0e7494fe24a34056972891128bb89
ZhuChongLin <8210856@qq.com>
2025-04-02 22:53:43 +0800

    feat(responsive): 添加响应式信号相关常量
    - 新增多个信号相关常量，用于标识不同类型的响应式信号
    - 包括基础信号、深度信号、非响应式信号、值信号和对象信号等常量

---
+++ packages/responsive/src/signal/constants.ts
@@ -0,0 +1,24 @@
+/**
+ * 响应式信号的基础标识符
+ */
+export const SIGNAL_SYMBOL = Symbol('VITARX_SIGNAL_SYMBOL')
+/**
+ * 深度信号标识符
+ */
+export const DEEP_SIGNAL_SYMBOL = Symbol('VITARX_DEEP_SIGNAL_SYMBOL')
+/**
+ * 非响应式信号标识符
+ */
+export const NOT_SIGNAL_SYMBOL = Symbol('VITARX_NOT_SIGNAL_SYMBOL')
+/**
+ * 值信号标识符
+ */
+export const VALUE_SIGNAL_SYMBOL = Symbol('VITARX_VALUE_SIGNAL_SYMBOL')
+/**
+ * 对象信号标识
+ */
+export const PROXY_SIGNAL_SYMBOL = Symbol('VITARX_OBJECT_SIGNAL_SYMBOL')
+/**
+ * 获取原始对象标识符
+ */
+export const GET_RAW_TARGET_SYMBOL = Symbol('VITARX_PROXY_RAW_TARGET_GET_SYMBOL')
