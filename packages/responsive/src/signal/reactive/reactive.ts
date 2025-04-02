3d76e7fe1eccfcc8c967f0852c68ca849f92a86e
ZhuChongLin <8210856@qq.com>
2025-04-02 22:37:17 +0800

    feat(responsive): 新增响应式对象相关函数
    - 添加 shallowReactive 函数，用于创建浅层响应式对象
    - 新增 isReactive 函数，用于判断对象是否为响应式对象
    - 实现 unReactive 函数，用于获取响应式对象的原始值
    - 同时提供 toRaw别名，以兼容 Vue API

--- packages/responsive/src/signal/reactive/reactive.ts
+++ packages/responsive/src/signal/reactive/reactive.ts
@@ -1,5 +1,6 @@
-import type { Reactive, ReactiveOptions, ShallowReactive } from './types'
+import type { Reactive, ReactiveOptions, ShallowReactive, UnReactive } from './types'
 import { createReactiveProxySignal } from './reactive-proxy-handler'
+import { PROXY_SIGNAL_SYMBOL } from '../constants'
 
 /**
  * ## 创建默认的深层响应式代理对象
@@ -46,3 +47,44 @@ export function reactive<T extends AnyObject>(
 ): Reactive<T> | ShallowReactive<T> {
   return createReactiveProxySignal(target, options)
 }
+
+/**
+ * ## 创建浅层响应式对象
+ *
+ * 该方法与`reactive({},{deep:false})`的效果是一致的。
+ *
+ * @template T - 目标对象类型
+ * @param { T } target - 目标对象
+ * @param { object } [options] - 代理选项
+ * @param { EqualityFn } [options.equalityFn=Object.is] - 自定义值比较函数
+ * @returns {ShallowReactive<T>} 浅层响应式对象
+ */
+export function shallowReactive<T extends AnyObject>(
+  target: T,
+  options?: Omit<ReactiveOptions, 'deep'>
+): ShallowReactive<T> {
+  return createReactiveProxySignal(target, { ...options, deep: false }) as ShallowReactive<T>
+}
+
+/**
+ * 判断是否为响应式对象
+ *
+ * @param val
+ */
+export function isReactive(val: unknown): boolean {
+  return typeof val === 'object' && val !== null && !!Reflect.get(val, PROXY_SIGNAL_SYMBOL)
+}
+
+/**
+ * ## 获取响应式对象的原始值，
+ *
+ * @template T - 目标对象类型
+ * @param { T | Reactive<T> } proxy - 代理对象
+ * @returns { UnReactive<T> } 如果传入的是 'reactive' 创建的对象，则会返回其真实的原始对象，否则原样返回。
+ * @alias toRaw 兼容 `Vue` api
+ */
+export function unReactive<T extends object>(proxy: T | Reactive<T>): UnReactive<T> {
+  return (Reflect.get(proxy, PROXY_SIGNAL_SYMBOL) ?? proxy) as UnReactive<T>
+}
+
+export { unReactive as toRaw }
