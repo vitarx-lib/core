0a7704be5827c6b797009a54e38341da8beecc8a
ZhuChongLin <8210856@qq.com>
2025-04-02 22:19:00 +0800

    refactor(responsive): 重构响应式代理对象处理器
    - 移除了不必要的类型引用
    - 添加了类型参数默认值
    - 简化了函数签名
    - 更新了文档注释

--- packages/responsive/src/signal/reactive/reactive-proxy-handler.ts
+++ packages/responsive/src/signal/reactive/reactive-proxy-handler.ts
@@ -2,8 +2,8 @@ import { isCollection, isObject } from '@vitarx/utils'
 import { isMarkNotSignal, isSignal, isValueSignal } from '../utils'
 import { Observer } from '../../observer/index'
 import { Depend } from '../../depend/index'
-import type { Reactive, ReactiveOptions, ShallowReactive, UnwrapNestedRefs } from './types'
-import type { BaseSignal, EqualityFn, ProxySignal } from '../types'
+import type { Reactive, ReactiveOptions, ShallowReactive } from './types'
+import type { BaseSignal, EqualityFn } from '../types'
 import {
   DEEP_SIGNAL_SYMBOL,
   GET_RAW_TARGET_SYMBOL,
@@ -13,7 +13,8 @@ import {
 import { SignalManager } from '../manager'
 
 /**
- * 响应式代理对象处理器
+ * # 响应式代理对象处理器
+ *
  * 实现了ES6 Proxy的处理器接口，用于创建响应式对象
  *
  * @template T - 目标对象类型，必须是一个对象类型
@@ -239,28 +240,14 @@ class ReactiveProxyHandler<T extends AnyObject> implements ProxyHandler<T> {
 }
 
 /**
- * 创建默认的深层响应式代理对象
- *
- * @template T - 目标对象类型
- * @param {T} target - 要代理的目标对象
- * @returns {Reactive<T>} 深层响应式代理对象
- * @example
- * ```typescript
- * const obj = { foo: { bar: 1 } }
- * const proxy = createReactiveProxy(obj)
- * // proxy.foo和proxy.foo.bar都是响应式的
- * ```
- */
-export function createReactiveProxySignal<T extends AnyObject>(target: T): Reactive<T>
-/**
- * 创建响应式代理对象，可自定义`equalityFn`
+ * ## 创建响应式代理信号
  *
  * @template T - 目标对象类型
  * @param {T} target - 要代理的目标对象
  * @param {object} options - 代理配置选项
- * @param {boolean} options.deep - 启用深度代理
+ * @param {boolean} [options.deep=true] - 启用深度代理
  * @param {EqualityFn} [options.equalityFn=Object.is] - 自定义值比较函数
- * @returns {ProxySignal<UnwrapNestedRefs<T>>} 深层响应式代理对象
+ * @returns {Reactive<T> | ShallowReactive<T>} 响应式代理对象
  * @example
  * ```typescript
  * const proxy = createReactiveProxy(target, {
@@ -269,14 +256,6 @@ export function createReactiveProxySignal<T extends AnyObject>(target: T): React
  * })
  * ```
  */
-export function createReactiveProxySignal<T extends AnyObject>(
-  target: T,
-  options: ReactiveOptions<true> | {} | undefined
-): Reactive<T>
-export function createReactiveProxySignal<T extends AnyObject>(
-  target: T,
-  options: ReactiveOptions<false>
-): ShallowReactive<T>
 export function createReactiveProxySignal<T extends AnyObject>(
   target: T,
   options?: ReactiveOptions
