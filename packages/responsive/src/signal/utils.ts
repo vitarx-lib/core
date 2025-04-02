d955d082b66ed09c80fcc5913adeca29656216f1
ZhuChongLin <8210856@qq.com>
2025-04-02 22:55:22 +0800

    feat(signal): 添加信号相关工具函数
    - 新增 isSignal、isValueSignal、isProxySignal、isDeepSignal 等函数用于检测信号类型
    - 添加 markNotSignal 和 isMarkNotSignal 函数用于标记和检测非信号对象
    - 提供 isProxy 和 markRaw别名以兼容 Vue API

---
+++ packages/responsive/src/signal/utils.ts
@@ -0,0 +1,85 @@
+import type { BaseSignal, NotSignal, ProxySignal, ValueSignal } from './types'
+import {
+  DEEP_SIGNAL_SYMBOL,
+  NOT_SIGNAL_SYMBOL,
+  PROXY_SIGNAL_SYMBOL,
+  SIGNAL_SYMBOL,
+  VALUE_SIGNAL_SYMBOL
+} from './constants'
+
+/**
+ * 是否为信号
+ *
+ * @param { any } val - 待检测的值
+ * @returns { boolean } - 如果具有信号标识符，则返回true，否则返回false
+ */
+export function isSignal(val: any): val is BaseSignal {
+  return val?.[SIGNAL_SYMBOL] === true
+}
+
+/**
+ * 是否为值信号
+ *
+ * @param { any } val - 待检测的变量
+ * @returns { boolean } - 如果具有值信号标识符，则返回true，否则返回false
+ */
+export function isValueSignal(val: any): val is ValueSignal {
+  return val?.[SIGNAL_SYMBOL] === true && val?.[VALUE_SIGNAL_SYMBOL] === true
+}
+
+/**
+ * 是否为代理对象信号
+ *
+ * @param { any } val - 待检测的变量
+ * @returns { boolean } - 如果具有对象信号标识符，则返回true，否则返回false
+ * @alias isProxy 兼容 `Vue` api
+ */
+export function isProxySignal(val: any): val is ProxySignal {
+  return val?.[SIGNAL_SYMBOL] === true && val?.[PROXY_SIGNAL_SYMBOL] === true
+}
+
+export { isProxySignal as isProxy }
+
+/**
+ * 是否为深度信号
+ *
+ * @param { BaseSignal } val - 待检测的信号
+ * @returns { boolean } - 如果具有深度信号标识符，则返回true，否则返回false
+ */
+export function isDeepSignal(val: BaseSignal): boolean {
+  return Reflect.get(val, DEEP_SIGNAL_SYMBOL)
+}
+
+/**
+ * 标记为非信号对象
+ *
+ * @alias markRaw 兼容 `Vue` api
+ * @template T - 待标记的对象类型
+ * @param { AnyObject } obj - 待标记的对象
+ * @returns { NotSignal<T> } - 返回标记后的对象
+ * @throws { TypeError } - 如果参数1不是对象类型，则抛出类型错误
+ */
+export function markNotSignal<T extends AnyObject>(obj: T): NotSignal<T> {
+  if (typeof obj !== 'object' || obj === null) {
+    throw new TypeError('[Vitarx.markNotSignal]: The argument must be an object type')
+  }
+  Object.defineProperty(obj, NOT_SIGNAL_SYMBOL, {
+    value: true
+  })
+  return obj as NotSignal<T>
+}
+
+export { markNotSignal as markRaw }
+
+/**
+ * 判断是否标记为非信号对象
+ *
+ * @param { AnyObject } obj - 待判断的对象
+ * @returns {boolean} - true表示标记为非信号对象
+ * @alias isMarkRaw 兼容 `Vue` api
+ */
+export function isMarkNotSignal(obj: AnyObject): boolean {
+  return !!(obj as any)[NOT_SIGNAL_SYMBOL]
+}
+
+export { isMarkNotSignal as isMarkRaw }
