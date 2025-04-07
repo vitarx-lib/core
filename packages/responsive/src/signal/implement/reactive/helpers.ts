14842f2306f9c06d57bebde437f91e7f6f7f2d2f
ZhuChongLin <8210856@qq.com>
2025-04-07 17:28:41 +0800

    refactor(responsive): 更新 import路径
    - 将 reactive-proxy-handler.ts 文件重命名为 proxy-handler.ts
    - 相应地更新了 import 路径

--- packages/responsive/src/signal/implement/reactive/helpers.ts
+++ packages/responsive/src/signal/implement/reactive/helpers.ts
@@ -1,5 +1,5 @@
 import { SIGNAL_RAW_VALUE_SYMBOL, type SignalOptions } from '../../core/index'
-import { createReactiveProxySignal, REACTIVE_PROXY_SYMBOL } from './reactive-proxy-handler'
+import { createReactiveProxySignal, REACTIVE_PROXY_SYMBOL } from './proxy-handler'
 import type { Reactive, Unreactive } from './types'
 
 /**
