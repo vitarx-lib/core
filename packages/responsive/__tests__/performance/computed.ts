dfa02abaaa2e68737f1a112a0505c2fbd8166c85
ZhuChongLin <8210856@qq.com>
2025-04-07 19:05:07 +0800

    perf(responsive): 添加计算属性性能测试
    - 新增计算属性性能测试脚本
    - 测试内容包括执行时间和内存增量
    - 使用 1000 个计算属性进行测试
    - 输出测试结果，包括执行时间和内存增量

---
+++ packages/responsive/__tests__/performance/computed.ts
@@ -0,0 +1,26 @@
+import { ref, watch } from '../../src/index'
+import { computed } from '../../src/signal/implement/computed/index'
+
+const { memoryUsage } = process
+
+const data = ref(1)
+const startMemory = memoryUsage()
+console.time('executionTime')
+
+const max = 1000
+let changeCount = 0
+
+for (let i = 0; i < max; i++) {
+  const c = computed(() => data.value + 100).init()
+  watch(c, () => changeCount++, { batch: false })
+}
+// 修改响应式变量
+data.value++
+Promise.resolve().then(() => {
+  console.log(`changeCount: ${changeCount}`)
+  console.timeEnd('executionTime')
+  const endMemory = memoryUsage()
+  // 算出rss总内存增量，MB单位
+  const memoryIncrement = (endMemory.rss - startMemory.rss) / 1024 / 1024
+  console.log(`内存增量为${memoryIncrement.toFixed(2)} MB`)
+})
