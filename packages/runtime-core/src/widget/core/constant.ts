b29e418878e222bb3c0380ff7f1dc2ad45da4040
ZhuChongLin <8210856@qq.com>
2025-07-29 22:23:08 +0800

    feat(runtime-core): 实现函数组件和类组件的基础结构
    - 添加了函数组件和类组件的基类和相关工具函数
    - 实现了组件的生命周期钩子和属性管理
    - 新增了简单函数组件的支持
    - 定义了组件内部保留的关键字和方法

---
+++ packages/runtime-core/src/widget/core/constant.ts
@@ -0,0 +1,60 @@
+/** 生命周期钩子枚举 */
+export enum LifecycleHooks {
+  create = 'onCreate',
+  beforeMount = 'onBeforeMount',
+  mounted = 'onMounted',
+  deactivated = 'onDeactivated',
+  activated = 'onActivated',
+  beforeUpdate = 'onBeforeUpdate',
+  updated = 'onUpdated',
+  error = 'onError',
+  unmounted = 'onUnmounted',
+  beforeUnmount = 'onBeforeUnmount',
+  beforeRemove = 'onBeforeRemove',
+  serverPrefetch = 'onServerPrefetch'
+}
+
+/**
+ * 组件内部保留方法
+ */
+export const __WIDGET_INTRINSIC_METHOD_KEYWORDS__ = [
+  'build',
+  'update',
+  ...Object.values(LifecycleHooks)
+] as const
+
+/**
+ * 组件内部保留属性
+ */
+export const __WIDGET_INTRINSIC_PROPERTY_KEYWORDS__ = [
+  'vnode',
+  'children',
+  'props',
+  'scope'
+] as const
+
+/**
+ * 组件内部保留关键字
+ */
+export const __WIDGET_INTRINSIC_KEYWORDS__ = [
+  ...__WIDGET_INTRINSIC_PROPERTY_KEYWORDS__,
+  ...__WIDGET_INTRINSIC_METHOD_KEYWORDS__
+] as const
+
+export const SIMPLE_FUNCTION_WIDGET_SYMBOL = Symbol('SIMPLE_FUNCTION_WIDGET_SYMBOL')
+
+/** 排除生命周期方法和保留属性 */
+export type ExcludeWidgetIntrinsicKeywords<T> = Omit<
+  T,
+  Exclude<(typeof __WIDGET_INTRINSIC_KEYWORDS__)[number], 'el'>
+>
+
+/**
+ * 初始化函数组件的标识符
+ */
+export const __INITIALIZE_FN_WIDGET__ = Symbol('__INITIALIZE_FN_WIDGET__')
+
+/**
+ * 类组件的标识符
+ */
+export const CLASS_WIDGET_BASE_SYMBOL = Symbol('CLASS_WIDGET_SYMBOL')
