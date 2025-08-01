39b1770903cbc05b656e956a1f7f41ad77a35691
ZhuChongLin <8210856@qq.com>
2025-08-01 12:46:01 +0800

    refactor(vnode): 优化虚拟节点相关类型定义
    - 添加VNode 类型导入，明确 Fragment 类型
    - 将 Fragment 类型从 any 改为 VNode，提高类型安全性

--- packages/runtime-core/src/vnode/core/constant.ts
+++ packages/runtime-core/src/vnode/core/constant.ts
@@ -1,9 +1,11 @@
+import type { VNode } from '../types/index'
+
 /** 虚拟节点标识符 */
 export const VNodeSymbol = Symbol('VNode')
 export type VNodeSymbol = typeof VNodeSymbol
 /** 片段组件标识符 */
-export const Fragment = Symbol('Fragment')
-export type Fragment = () => any
+export const Fragment = Symbol('Fragment') as unknown as () => VNode
+export type Fragment = () => VNode
 
 /** 元素引用标记 */
 export const RefElSymbol = Symbol('RefEl')
