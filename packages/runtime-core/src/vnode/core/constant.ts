63ac8fe7cdb6397e60dfe8d75ff8316179220e93
ZhuChongLin <8210856@qq.com>
2025-08-02 19:06:14 +0800

    refactor(vnode): 重构 Fragment 实现
    - 将 Fragment从函数改为字符串常量
    - 调整 Fragment 类型定义
    - 移除未使用的VNode 引入

--- packages/runtime-core/src/vnode/core/constant.ts
+++ packages/runtime-core/src/vnode/core/constant.ts
@@ -1,12 +1,9 @@
-import type { VNode } from '../types/index'
-
 /** 虚拟节点标识符 */
 export const VNodeSymbol = Symbol('VNode')
 export type VNodeSymbol = typeof VNodeSymbol
 /** 片段组件标识符 */
-export const Fragment = Symbol('Fragment') as unknown as () => VNode
-export type Fragment = () => VNode
-
+export const Fragment = 'fragment-node'
+export type Fragment = typeof Fragment
 /** 元素引用标记 */
 export const RefElSymbol = Symbol('RefEl')
 export type RefElSymbol = typeof RefElSymbol
