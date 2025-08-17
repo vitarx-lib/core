3a7cf63a7cd60b58985442c2a5b7d9ca7ee6b1f7
ZhuChongLin <8210856@qq.com>
2025-08-18 00:19:58 +0800

    refactor(vnode): 重构虚拟节点创建逻辑
    - 优化 createVNode 函数，支持 JSX语法
    - 添加对简单组件的直接支持
    - 改进 FragmentVNode 和 ElementVNode 的创建方式
    - 增加对 children 属性的处理，支持多种数据类型

--- packages/runtime-core/src/vnode/helper.ts
+++ packages/runtime-core/src/vnode/helper.ts
@@ -1,3 +1,4 @@
+import { isSimpleWidget } from '../widget/index'
 import { CommentVNode, ElementVNode, FragmentVNode, TextVNode, VNode, WidgetVNode } from './nodes'
 import { type Child, type VNodeProps, VNodeType } from './types'
 
@@ -6,7 +7,7 @@ import { type Child, type VNodeProps, VNodeType } from './types'
  *
  * @param type - 虚拟节点的类型，可以是字符串或对象
  * @param props - 虚拟节点的属性
- * @param children - 虚拟节点的子节点
+ * @param children - 虚拟节点的子节点，兼容jsx:react模式
  * @returns {VNode} 返回创建的虚拟节点实例
  */
 export function createVNode<T extends VNodeType>(
@@ -14,6 +15,18 @@ export function createVNode<T extends VNodeType>(
   props: VNodeProps<T> | null = null,
   ...children: Child[]
 ): VNode<T> {
+  props ??= {} as VNodeProps<T>
+  if (children) {
+    if (props!.children) {
+      if (Array.isArray(props!.children)) {
+        props!.children.push(...children)
+      } else {
+        props!.children = [props!.children, ...children]
+      }
+    } else {
+      props!.children = children
+    }
+  }
   // 如果类型是字符串，则根据不同的类型创建不同类型的虚拟节点
   if (typeof type === 'string') {
     switch (type) {
@@ -21,26 +34,29 @@ export function createVNode<T extends VNodeType>(
       case 'text-node':
       case 'comment-node':
         let value: string = ''
-        // 从props或children中获取节点的值
-        if (typeof props?.children === 'string') {
-          value = props.children
-        } else if (typeof children[0] === 'string') {
-          value = children[0]
+        if (props && 'children' in props) {
+          if (typeof props.children === 'string') {
+            value = props.children
+          } else if (Array.isArray(props.children) && typeof props.children[0] === 'string') {
+            value = props.children[0]
+          }
         }
         // 根据类型创建文本节点或注释节点
         // 处理片段节点
         return new (type === 'text-node' ? TextVNode : CommentVNode)(value) as unknown as VNode<T>
       case 'fragment-node':
         // 默认处理元素节点
-        return new FragmentVNode(props, children) as unknown as VNode<T>
+        return new FragmentVNode(props) as unknown as VNode<T>
       default:
-        return new ElementVNode(type, props, children) as unknown as VNode<T>
+        return new ElementVNode(type, props) as unknown as VNode<T>
     }
-    // 如果类型不是字符串，则创建组件节点
   }
-  return new WidgetVNode(type, props, children) as unknown as VNode<T>
+  if (isSimpleWidget(type)) {
+    const vnode = type.call(null, props)
+    if (VNode.is(vnode)) return vnode as unknown as VNode<T>
+  }
+  return new WidgetVNode(type, props) as unknown as VNode<T>
 }
-
 /**
  * 获取当前虚拟节点(WidgetVNode)的函数
  * 该函数通过调用WidgetVNode类的静态方法getCurrentVNode()来实现
