14d59f9a57772ff970adda09a59c37fb973c062d
ZhuChongLin <8210856@qq.com>
2025-08-18 02:32:01 +0800

    feat(runtime-core): 实现 JSX 开发模式和运行时模块
    - 新增 jsx-dev-runtime.ts 文件，实现 JSX 开发模式下的 VNode 构建
    - 新增 jsx-runtime.ts 文件，实现 JSX 运行时的 VNode 构建
    - 添加 Fragment 导出
    - 实现热模块替换功能

---
+++ packages/runtime-core/src/jsx-runtime.ts
@@ -0,0 +1,30 @@
+import {
+  createVNode,
+  Fragment,
+  type UniqueKey,
+  VNode,
+  type VNodeProps,
+  type VNodeType
+} from './vnode'
+
+/**
+ * JSX构建VNode
+ *
+ * @see createVNode
+ */
+function jsx<T extends VNodeType>(
+  type: T,
+  props: VNodeProps<T> | null = null,
+  key?: UniqueKey
+): VNode<T> {
+  if (key) {
+    if (!props) {
+      props = { key } as any
+    } else {
+      props.key = key
+    }
+  }
+  return createVNode(type, props)
+}
+
+export { jsx, Fragment, jsx as jsxs }
