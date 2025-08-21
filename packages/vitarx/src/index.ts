915667180f856901f970b8804d4e6b6b26b22993
ZhuChongLin <8210856@qq.com>
2025-08-21 21:53:14 +0800

    refactor(vitarx): 重构并增强类型定义
    - 在 index.ts 中添加了 Vitarx 和 JSX 命名空间的全局类型声明
    - 新增了多个类型别名，以提供更清晰的代码和更好的类型检查
    - 例如：CssStyle, HtmlStyleProperties, HtmlClassProperties, FnWidget, TSWidget 等
    - 这些类型定义增强了对 CSS 样式、HTML 属性、函数式小部件等的类型检查和代码提示

--- packages/vitarx/src/index.ts
+++ packages/vitarx/src/index.ts
@@ -1,4 +1,76 @@
+import type {
+  ClassProperties,
+  FunctionWidget,
+  IntrinsicNodeElement,
+  IntrinsicProperties,
+  StyleProperties,
+  StyleRules,
+  TSWidget as _TSWidget,
+  VNode,
+  VNodeType,
+  Widget
+} from '@vitarx/runtime-core'
+
 export * from '@vitarx/utils'
 export * from '@vitarx/responsive'
 export * from '@vitarx/runtime-core'
 export * from './app'
+
+declare global {
+  namespace Vitarx {
+    /**
+     * CSS样式类型映射
+     */
+    type CssStyle = StyleRules
+    /**
+     * HTML style 属性类型
+     */
+    type HtmlStyleProperties = StyleProperties
+    /**
+     * HTML class 属性类型
+     */
+    type HtmlClassProperties = ClassProperties
+    /**
+     * 函数式小部件函数类型
+     */
+    type FnWidget = FunctionWidget
+    /**
+     * TSX 类型支持工具
+     *
+     * 将不被TSX支持的组件类型（如：异步组件，懒加载组件），重载为受支持的TSX组件类型，提供友好的参数类型校验。
+     *
+     * @example
+     * ```tsx
+     * async function AsyncWidget() {
+     *   await new Promise((resolve) => setTimeout(resolve, 1000))
+     *   return <div>Hello World</div>
+     * }
+     * export default AsyncWidget
+     * <AsyncWidget /> // ❌ TSX 语法校验不通过！
+     *
+     * export default AsyncWidget as unknown as Vitarx.TSWidget<typeof AsyncWidget>
+     * <AsyncWidget /> // ✅ TSX 语法校验通过！
+     * ```
+     */
+    type TSWidget<T extends FunctionWidget> = _TSWidget<T>
+    /** 兼容JSX，同VNode类型一致 */
+    type Element<T extends VNodeType = VNodeType> = VNode<T>
+    /** 类组件实例 */
+    type ElementClass = Widget
+  }
+  namespace JSX {
+    /** 元素类型 */
+    type Element = Vitarx.Element
+    /** 类组件实例 */
+    type ElementClass = Vitarx.ElementClass
+
+    /** 固有元素 */
+    interface IntrinsicElements extends IntrinsicNodeElement {}
+
+    /** 固有属性 */
+    interface IntrinsicAttributes extends IntrinsicProperties {}
+
+    /** 子节点类型校验 */
+    export interface ElementChildrenAttribute {}
+  }
+}
