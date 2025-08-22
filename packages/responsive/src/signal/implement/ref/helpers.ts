4455a4d0f75b54d35f0eadb523254769d90b3557
ZhuChongLin <8210856@qq.com>
2025-08-22 14:58:55 +0800

    refactor(responsive): 优化 ref 和 shallowRef 函数的 TypeScript定义
    - 为 ref函数添加多个重载定义，以支持不同的使用场景
    - 为 shallowRef 函数添加重载定义，以支持不同的使用场景
    - 优化了函数参数和返回值的类型定义，提高了类型安全性

--- packages/responsive/src/signal/implement/ref/helpers.ts
+++ packages/responsive/src/signal/implement/ref/helpers.ts
@@ -8,6 +8,41 @@ import { Ref } from './ref'
  * 所有依赖于该引用的计算和副作用将自动更新。默认情况下，对嵌套对象进行深度代理。
  *
  * @template Value - 信号值的类型
+ * @returns {Ref<Value | undefined, true>} - 创建的响应式引用信号
+ */
+export function ref(): Ref<any>
+/**
+ * 创建一个响应式引用信号
+ *
+ * 创建一个包装对象，使其成为响应式数据源。当引用的值发生变化时，
+ * 所有依赖于该引用的计算和副作用将自动更新。默认情况下，对嵌套对象进行深度代理。
+ *
+ * @template Value - 信号值的类型
+ * @returns {Ref<Value | undefined, true>} - 创建的响应式引用信号
+ */
+export function ref<Value>(): Ref<Value | undefined>
+/**
+ * 创建一个响应式引用对象，用于在响应式系统中包装原始值或引用
+ *
+ * @template Value - 引用中存储的值的类型
+ * @template Deep - 是否深度转换对象的布尔值，默认为true
+ * @param value - 要包装的初始值，可以是普通值或已有的引用
+ * @param {object | boolean} [options] - 信号的选项配置，支持直接传入boolean指定deep配置
+ * @param {boolean} [options.deep=true] - 是否深度代理嵌套对象，为true时会递归代理所有嵌套属性
+ * @param {function} [options.compare=Object.is] - 值比较函数，用于决定是否触发更新，默认使用Object.is进行比较
+ * @returns - 返回一个响应式引用对象，该对象会跟踪其值的变化
+ */
+export function ref<Value, Deep extends boolean = true>(
+  value: Value | Ref<Value, Deep>, // 要包装的值，可以是原始值或已有的引用
+  options?: SignalOptions<Deep> | Deep // 可选配置，可以是SignalOptions对象或布尔值
+): Ref<Value, Deep> // 返回一个响应式引用对象
+/**
+ * 创建一个响应式引用信号
+ *
+ * 创建一个包装对象，使其成为响应式数据源。当引用的值发生变化时，
+ * 所有依赖于该引用的计算和副作用将自动更新。默认情况下，对嵌套对象进行深度代理。
+ *
+ * @template Value - 信号值的类型
  * @template Deep - 是否使用深度信号，默认为true
  * @param {Value} [value=undefined] - 信号初始值
  * @param {object | boolean} [options] - 信号的选项配置，支持直接传入boolean指定deep配置
@@ -48,6 +83,30 @@ export function ref<Value = any, Deep extends boolean = true>(
   return new Ref(value, options) as Ref<Value, Deep>
 }
 
+/**
+ * 创建一个浅层响应式引用对象
+ *
+ * @returns 返回一个浅层响应式引用对象，初始值为 undefined
+ */
+export function shallowRef(): Ref<any, false>
+/**
+ * 创建一个浅层响应式引用对象
+ *
+ * @template Value - 引用值类型
+ * @returns 返回一个浅层响应式引用对象，初始值为 undefined
+ */
+export function shallowRef<Value>(): Ref<Value | undefined, false>
+/**
+ * 创建一个浅层响应式引用对象
+ * @param value - 初始值
+ * @param {object} [options] - 可选配置项
+ * @param {function} [options.compare=Object.is] - 值比较函数，用于决定是否触发更新
+ * @returns - 返回一个浅层响应式引用对象
+ */
+export function shallowRef<Value>(
+  value: Value | Ref<Value, false>, // 初始值，可以是普通值或非深层的响应式引用
+  options?: Omit<SignalOptions, 'deep'> // 可选配置，排除了 'deep' 选项
+): Ref<Value, false> // 返回一个浅层响应式引用对象
 /**
  * 创建一个浅响应式引用信号
  *
