42980fe070aa691fe18914c81f4da9ea815d05a9
ZhuChongLin <8210856@qq.com>
2025-08-27 16:16:17 +0800

    refactor(vnode): 更新 Text 和 Comment 类型定义以支持 RefSignal
    - 在 Text 和 Comment 的类型定义中添加对 RefSignal 的支持
    - 这个改动使得它们能够处理响应式的数据，提高了灵活性和适用性

--- packages/runtime-core/src/vnode/constant.ts
+++ packages/runtime-core/src/vnode/constant.ts
@@ -1,3 +1,4 @@
+import type { RefSignal } from '@vitarx/responsive'
 import { VNode } from './nodes'
 import type { Child } from './types'
 
@@ -14,7 +15,9 @@ export type Fragment = typeof Fragment
  *
  * 通常你无需使用此标识符创建VNode，而是直接使用字符串作为子节点
  */
-export const Text = 'text-node' as unknown as (props: { children: string }) => VNode
+export const Text = 'text-node' as unknown as (props: {
+  children: string | RefSignal<string>
+}) => VNode
 
 export type Text = typeof Text
 
@@ -23,5 +26,7 @@ export type Text = typeof Text
  *
  * 由于jsx特性，需要将注释展示到DOM树中，则需要使用<Comment>/<comment-node>标识符做为元素
  */
-export const Comment = 'comment-node' as unknown as (props: { children: string }) => VNode
+export const Comment = 'comment-node' as unknown as (props: {
+  children: RefSignal<string>
+}) => VNode
 export type Comment = typeof Comment
