41f885836ee5a72ef8a088f09e8a98c421cf0034
ZhuChongLin <8210856@qq.com>
2025-08-29 13:14:14 +0800

    refactor(vnode): 优化VNode 类型定义
    - 为 Fragment、Text 和 Comment 添加专用类型定义
    - 在类型定义中添加 __isFragment__、__isText__ 和 __isComment__ 标记
    - 优化类型定义的结构，提高可读性和可维护性

--- packages/runtime-core/src/vnode/constant.ts
+++ packages/runtime-core/src/vnode/constant.ts
@@ -2,31 +2,32 @@ import type { RefSignal } from '@vitarx/responsive'
 import { VNode } from './nodes'
 import type { Child } from './types'
 
+type FragmentType = ((props: { children?: Child | Child[] }) => VNode) & { __isFragment__: true }
 /**
  * 片段元素标识符
  */
-export const Fragment = 'fragment-node' as unknown as (props: {
-  children?: Child | Child[]
-}) => VNode
+export const Fragment = 'fragment-node' as unknown as FragmentType
 
 export type Fragment = typeof Fragment
+
+type TextType = ((props: { children: string | RefSignal<string> }) => VNode) & {
+  __isText__: true
+}
 /**
  * 文本元素标识符
  *
  * 通常你无需使用此标识符创建VNode，而是直接使用字符串作为子节点
  */
-export const Text = 'text-node' as unknown as (props: {
-  children: string | RefSignal<string>
-}) => VNode
-
+export const Text = 'text-node' as unknown as TextType
 export type Text = typeof Text
 
+type CommentType = ((props: { children: string | RefSignal<string> }) => VNode) & {
+  __isComment__: true
+}
 /**
  * 注释元素标识符
  *
  * 由于jsx特性，需要将注释展示到DOM树中，则需要使用<Comment>/<comment-node>标识符做为元素
  */
-export const Comment = 'comment-node' as unknown as (props: {
-  children: RefSignal<string>
-}) => VNode
+export const Comment = 'comment-node' as unknown as CommentType
 export type Comment = typeof Comment
