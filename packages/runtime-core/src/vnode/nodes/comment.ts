c79ac3250b356997cd5a4bb357a91dc73437385b
ZhuChongLin <8210856@qq.com>
2025-09-06 01:41:28 +0800

    refactor(runtime-core): 为 CommentVNode 类添加文档注释
    - 在 CommentVNode 类上添加了详细的 JSDoc 注释
    - 说明了类的用途、功能和使用示例
    - 添加了参数和继承关系的注释

--- packages/runtime-core/src/vnode/nodes/comment.ts
+++ packages/runtime-core/src/vnode/nodes/comment.ts
@@ -1,6 +1,26 @@
 import { RefSignal } from '@vitarx/responsive'
 import { NoTagVNode } from './no-tag.js'
 
+/**
+ * 表示一个注释节点的虚拟节点类，用于在DOM中创建和管理注释节点。
+ * 该类继承自NoTagVNode，专门处理HTML注释节点的创建和渲染。
+ *
+ * 主要功能：
+ * - 创建和管理DOM注释节点
+ * - 支持静态字符串和响应式字符串作为注释内容
+ * - 提供注释节点的类型检查功能
+ *
+ * @example
+ * // 创建一个静态注释节点
+ * const staticComment = new CommentVNode('This is a comment');
+ *
+ * // 创建一个响应式注释节点
+ * const reactiveComment = new CommentVNode(ref('Dynamic comment'));
+ *
+ * @param value - 注释内容，可以是静态字符串或响应式字符串(RefSignal<string>)
+ *
+ * @extends NoTagVNode<'comment-node'>
+ */
 export class CommentVNode extends NoTagVNode<'comment-node'> {
   /**
    * 运行时元素实例
