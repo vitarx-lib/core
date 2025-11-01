4c3ddb68f20cafef42d30690a01cae2762c28547
ZhuChongLin <8210856@qq.com>
2025-11-02 01:23:14 +0800

    feat(runtime-core): 添加属性标准化和解包功能
    - 新增 normalizeStyle 函数用于标准化样式属性
    - 实现 unwrapRefProps 函数用于解包 ref 属性
    - 合并 class 和 className 属性处理逻辑
    - 删除独立的 unwrapRefProps 文件，整合到 normalizeProps 中
    - 添加详细的 JSDoc 注释说明函数用途和参数
    - 使用 unref 函数处理响应式属性值

--- packages/runtime-core/src/vnode/utils/normalizeProps.ts
+++ packages/runtime-core/src/vnode/utils/normalizeProps.ts
@@ -1,9 +1,17 @@
+import { unref } from '@vitarx/responsive'
 import type { AnyProps, ClassAttribute, StyleRules } from '../../types/index.js'
 import { StyleUtils } from './style.js'
 
+/**
+ * 标准化样式属性
+ * 该函数用于处理传入的props中的style和class属性，将其转换为统一的格式
+ * @param props 包含任意属性的输入对象
+ * @returns {object} 返回处理后的标准化属性对象
+ */
 export function normalizeStyle<T extends AnyProps>(props: T): T {
   // 处理 style 属性
   if ('style' in props) {
+    // 将样式值转换为对象格式
     ;(props as unknown as Record<'style', StyleRules>).style = StyleUtils.cssStyleValueToObject(
       props.style
     )
@@ -12,11 +20,29 @@ export function normalizeStyle<T extends AnyProps>(props: T): T {
   let cssClass: ClassAttribute =
     'class' in props ? StyleUtils.cssClassValueToArray(props.class as ClassAttribute) : []
   if ('className' in props) {
+    // 合并class和className属性
     cssClass = StyleUtils.mergeCssClass(cssClass, props.className as ClassAttribute)
-    // @ts-ignore
+    // @ts-ignore - 删除className属性，因为已经合并到class中
     delete this.props.className
   }
   // 如果合并后的 class 存在，赋值给 newProps.class
   if (cssClass.length > 0) (props as unknown as Record<'class', string[]>).class = cssClass
+  return props
+}
+
+/**
+ * 解包ref属性函数
+ * 该函数会遍历传入的对象的所有属性，并将每个属性通过unref函数进行解包
+ * @param props - 需要解包的属性对象，类型为AnyProps
+ * @returns {object} 解包后的属性对象
+ */
+export function unwrapRefProps<T extends AnyProps>(props: T): T {
+  // 检查props对象是否包含属性
+  if (Object.keys(props).length) {
+    // 解包ref
+    for (const prop in props) {
+      props[prop] = unref(props[prop])
+    }
+  }
   return props
 }
