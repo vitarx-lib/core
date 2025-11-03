6c2a9e3dbb5dfdf1b263b2b81f12f913f7c33de5
ZhuChongLin <8210856@qq.com>
2025-11-04 05:58:38 +0800

    refactor(widget): 重命名内置组件目录并移除Teleport导出
    - 将built目录重命名为widgets
    - 从widgets导出中移除Teleport组件
    - 更新index.ts中的导入路径引用新目录名

--- packages/runtime-core/src/widget/index.ts
+++ packages/runtime-core/src/widget/index.ts
@@ -1,4 +1,4 @@
 export * from './constants/index.js'
 export * from './utils/index.js'
 export * from './base/index.js'
-export * from './built/index.js'
+export * from './widgets/index.js'
--- packages/runtime-core/src/widget/widgets/index.ts
+++ packages/runtime-core/src/widget/widgets/index.ts
@@ -1,5 +1,4 @@
 export * from './Suspense.js'
 export * from './KeepAlive.js'
 export * from './LazyWidget.js'
-export * from './Teleport.js'
 export * from './PropBind.js'
