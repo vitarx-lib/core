9433f48a8ecd07448c55425ca958a8cf8a0713f5
ZhuChongLin <8210856@qq.com>
2025-11-05 04:02:08 +0800

    refactor(vnode): 重构 RegularElementNode 继承结构和初始化逻辑
    - 将 RegularElementNode 从继承 ContainerNode 改为继承 ElementNode
    - 移除 handleShowState 和 normalizeProps 方法
    - 在构造函数中处理 children 属性的提取与规范化
    - 引入 mixinContainerNode 实现容器节点功能
    - 更新类型定义引用和相关导入模块
    - 删除旧的 createElement 方法实现
    - 调整类注释以反映新的继承关系和功能描述

--- packages/runtime-core/src/vnode/nodes/RegularElementNode.ts
+++ packages/runtime-core/src/vnode/nodes/RegularElementNode.ts
@@ -1,59 +1,48 @@
+import { unref } from '@vitarx/responsive'
+import { popProperty } from '@vitarx/utils'
 import type {
-  NodeElementType,
-  NodeNormalizedProps,
-  RegularElementNodeType
+  RegularElementNodeType,
+  RuntimeVNodeChildren,
+  VNodeInputProps
 } from '../../types/index.js'
-import { ContainerNode } from '../base/index.js'
+import { type ContainerNode, ElementNode, mixinContainerNode } from '../base/index.js'
 import { NodeShapeFlags } from '../constants/index.js'
-import { normalizeStyle } from '../utils/normalizeProps.js'
+import { normalizeChildren } from '../utils/normalize.js'
 
 /**
  * RegularElementNode 是一个用于表示虚拟DOM中元素节点的类。
- * 它继承自 ContainerNode，提供了元素节点的基础功能。
+ * 它继承自 ElementNode，提供了元素节点的基础功能。
  *
  * 主要功能：
  * - 处理元素的显示/隐藏状态
  * - 规范化元素的属性
  * - 创建实际的DOM元素实例
  *
- * @template T - 元素节点类型，默认为 ElementNodeType
+ * @template T - 元素节点类型，默认为 RegularElementNodeType
  *
- * @example
- * ```js
- * // 创建一个元素节点
- * const vnode = new ElementVNode('div', { class: 'container' })
- * ```
- *
- * @extends ContainerNode<T>
+ * @extends ElementNode<T>
  *
  * @note
  * - 此类主要用于框架内部的虚拟DOM实现
  * - 不应直接实例化，而是通过框架的创建函数来使用
  * - 元素的属性会被自动规范化处理
  */
-export class RegularElementNode<
-  T extends RegularElementNodeType = RegularElementNodeType
-> extends ContainerNode<T> {
+export class RegularElementNode<T extends RegularElementNodeType = RegularElementNodeType>
+  extends ElementNode<T>
+  implements ContainerNode
+{
   public override shapeFlags = NodeShapeFlags.REGULAR_ELEMENT
   /**
-   * @inheritDoc
+   * 子节点列表
    */
-  protected override handleShowState(visible: boolean): void {
-    // 如果显示，则恢复style样式，如果没有则恢复为空
-    this.dom.setDisplay(this.element, visible)
-  }
-  /**
-   * @inheritDoc
-   */
-  protected override normalizeProps(props: Record<string, any>): NodeNormalizedProps<T> {
-    return normalizeStyle(super.normalizeProps(props))
-  }
-  /**
-   * @inheritDoc
-   */
-  protected createElement(): NodeElementType<T> {
-    const element = this.dom.createElement(this.type, this.props)
-    if (!this.show) this.dom.setDisplay(element, false)
-    return element as NodeElementType<T>
+  public children: RuntimeVNodeChildren
+
+  constructor(type: T, props: VNodeInputProps<T>) {
+    const hasChildren = 'children' in props
+    const children = hasChildren ? unref(popProperty(props, 'children')) : undefined
+    super(type, props)
+    // 如果存在children属性，则格式化子节点 // TODO 递归完善 svg
+    this.children = hasChildren ? normalizeChildren(children, this) : []
+    mixinContainerNode(this)
   }
 }
