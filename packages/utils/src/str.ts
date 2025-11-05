2fc9a7e3facf5a52102a547850050b8b1adf9fba
ZhuChongLin <8210856@qq.com>
2025-11-06 01:50:40 +0800

    feat(utils): 添加字符串首字母大写转换函数
    - 新增 toCapitalize 函数用于将字符串首字母大写
    - 为 toCamelCase 和 toKebabCase 函数添加使用示例
    - 增强类型支持，toCapitalize 返回类型为 Capitalize<T>

--- packages/utils/src/str.ts
+++ packages/utils/src/str.ts
@@ -3,6 +3,10 @@
  *
  * @param str - 输入的字符串，通常为短横线分隔的格式
  * @returns {string} 转换后的驼峰命名格式字符串
+ * @example
+ * ```js
+ * toCamelCase('hello-world') // 输出：'helloWorld'
+ * ```
  */
 export function toCamelCase(str: string): string {
   // 使用正则表达式匹配短横线后的字母，并将其转换为大写
@@ -14,8 +18,27 @@ export function toCamelCase(str: string): string {
  *
  * @param str - 需要转换的驼峰命名字符串
  * @returns {string} 转换后的短横线命名字符串
+ * @example
+ * ```js
+ * toKebabCase('helloWorld') // 输出：'hello-world'
+ * ```
  */
 export function toKebabCase(str: string): string {
   // 使用正则表达式匹配所有大写字母，并在前面添加短横线，然后转换为小写
   return str.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`)
+}
+
+/**
+ * 将字符串首字母大写
+ * @param str 需要处理的字符串
+ * @returns {string} 返回首字母大写后的字符串
+ * @example
+ * ```js
+ * toCapitalize('hello world') // 输出：'Hello world'
+ * ```
+ */
+export function toCapitalize<T extends string>(str: T): Capitalize<T> {
+  // 获取字符串的第一个字符并将其转换为大写
+  // 然后将其与字符串的其余部分（从第二个字符开始）拼接
+  return (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>
 }
