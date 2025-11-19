5b9ed630c87edc97dd89a16157b67eded3e44b95
ZhuChongLin <8210856@qq.com>
2025-11-19 17:39:59 +0800

    feat(runtime-core): 新增createVNode函数实现虚拟节点创建
    - 添加createDynamicVNode函数处理动态组件节点
    - 实现createVNode函数区分处理字符串类型节点和组件节点
    - 对不支持children的节点属性进行开发环境的警告提示
    - 支持文本、注释、片段、常规元素和空元素的虚拟节点创建
    - 集成组件节点判断并调用相应创建函数
    - 异常处理无效节点类型时抛出错误
    - 导出createVNode函数别名h供外部使用

---
+++ packages/runtime-core/src/vnode/create.ts
@@ -0,0 +1,86 @@
+import { unref } from '@vitarx/responsive'
+import { logger } from '@vitarx/utils'
+import {
+  COMMENT_NODE_TYPE,
+  DYNAMIC_RENDER_TYPE,
+  FRAGMENT_NODE_TYPE,
+  TEXT_NODE_TYPE
+} from '../constants/index.js'
+import { __DEV__, getNodeDevInfo } from '../internal/dev.js'
+import type {
+  TextVNodeType,
+  ValidNodeProps,
+  ValidNodeType,
+  VNode,
+  VNodeInstanceType
+} from '../types/index.js'
+import { isSupportChildren, isWidget } from '../utils/index.js'
+import {
+  createCommentNode,
+  createFragmentNode,
+  createRegularElementNode,
+  createTextNode,
+  createVoidElementNode
+} from './hostNodeCreator.js'
+import { createWidgetNode } from './widgetNodeCreator.js'
+
+/**
+ * 处理动态组件（DYNAMIC_RENDER_TYPE）
+ */
+function createDynamicVNode(props: Record<string, any>): VNode {
+  const { is: dynamicWidget, ...dynamicProps } = props
+  const renderNodeType = unref(dynamicWidget)
+  if (!renderNodeType) {
+    throw new Error('dynamic render "is" prop is mandatory and cannot be empty.')
+  }
+  return createVNode(renderNodeType, dynamicProps)
+}
+
+/**
+ * 创建虚拟节点
+ *
+ * @param type - 节点类型
+ * @param props - 节点属性
+ * @returns VNode实例
+ */
+export function createVNode<T extends ValidNodeType>(
+  type: T,
+  props: ValidNodeProps<T> = {} as ValidNodeProps<T>
+): VNodeInstanceType<T> {
+  const supportChildren = isSupportChildren(type)
+  // 检查不支持children的节点
+  if (__DEV__ && !supportChildren && 'children' in props) {
+    const devInfo = getNodeDevInfo(props)
+    logger.warn(`<${type}> children prop will be ignored`, devInfo?.source)
+    delete props.children
+  }
+
+  // 处理字符串类型节点
+  if (typeof type === 'string') {
+    switch (type) {
+      case DYNAMIC_RENDER_TYPE:
+        return createDynamicVNode(props) as VNodeInstanceType<T>
+      case TEXT_NODE_TYPE:
+        return createTextNode(props as ValidNodeProps<TextVNodeType>) as VNodeInstanceType<T>
+      case COMMENT_NODE_TYPE:
+        return createCommentNode(props as ValidNodeProps<TextVNodeType>) as VNodeInstanceType<T>
+      case FRAGMENT_NODE_TYPE:
+        return createFragmentNode(props) as VNodeInstanceType<T>
+      default:
+        if (supportChildren) {
+          return createRegularElementNode(type, props as never) as VNodeInstanceType<T>
+        } else {
+          return createVoidElementNode(type, props as never) as VNodeInstanceType<T>
+        }
+    }
+  }
+
+  // 处理组件节点
+  if (isWidget(type)) {
+    return createWidgetNode(type, props) as VNodeInstanceType<T>
+  }
+
+  throw new Error('createVNode(): invalid node type')
+}
+
+export { createVNode as h }
