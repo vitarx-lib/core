322213a76b8c6cc503762791c2fc33d9ef47aebb
ZhuChongLin <8210856@qq.com>
2025-11-19 18:21:21 +0800

    refactor(runtime-core): 统一VNode类型判断逻辑
    - 统一使用NodeKind替代shapeFlags判断节点类型
    - 更新VNode相关类型导入路径
    - 优化isXXXNode系列函数，全部基于kind字段判断
    - 删除废弃的isSupportChildren功能
    - 简化代码结构，提高类型判断准确性和维护性

--- packages/runtime-core/src/utils/vnode.ts
+++ packages/runtime-core/src/utils/vnode.ts
@@ -1,26 +1,17 @@
-import { useDomAdapter } from '../adapter/index.js'
-import {
-  COMMENT_NODE_TYPE,
-  DYNAMIC_RENDER_TYPE,
-  FRAGMENT_NODE_TYPE,
-  NodeShapeFlags,
-  TEXT_NODE_TYPE,
-  VIRTUAL_NODE_SYMBOL
-} from '../constants/index.js'
-import type { ValidNodeType } from '../types/index.js'
-import {
-  CommentNode,
-  type ContainerNode,
-  ElementNode,
-  FragmentNode,
-  RegularElementNode,
-  StatefulWidgetNode,
-  StatelessWidgetNode,
-  TextNode,
+import { NodeKind, VIRTUAL_NODE_SYMBOL } from '../constants/index.js'
+import type {
+  CommentVNode,
+  ContainerVNode,
+  ElementVNode,
+  FragmentVNode,
+  RegularElementVNode,
+  StatefulWidgetVNode,
+  StatelessWidgetVNode,
+  TextVNode,
   VNode,
   VoidElementVNode,
-  WidgetNode
-} from '../vnode/index.js'
+  WidgetVNode
+} from '../types/index.js'
 
 /**
  * 检查给定的值是否为虚拟节点(VNode)
@@ -31,64 +22,54 @@ import {
 export function isVNode(val: any): val is VNode {
   return val && typeof val === 'object' && val[VIRTUAL_NODE_SYMBOL] === true
 }
-
 /**
  * 检查传入的值是否为WidgetNode类型
  *
  * @param val - 需要检查的值
  * @returns {boolean} 如果值是WidgetNode类型则返回true，否则返回false
  */
-export function isStatefulWidgetNode(val: any): val is StatefulWidgetNode {
-  return isVNode(val) && val.shapeFlags === NodeShapeFlags.STATEFUL_WIDGET
+export function isStatefulWidgetNode(val: any): val is StatefulWidgetVNode {
+  return isVNode(val) && val.kind === NodeKind.STATEFUL_WIDGET
 }
-
 /**
  * 检查给定的值是否为简单组件节点(StatelessWidgetNode)
  *
  * @param val - 需要检查的值
  * @returns {boolean} 如果值是SimpleWidgetNode类型则返回true，否则返回false
  */
-export function isStatelessWidgetNode(val: any): val is StatelessWidgetNode {
-  return isVNode(val) && val.shapeFlags === NodeShapeFlags.STATELESS_WIDGET
+export function isStatelessWidgetNode(val: any): val is StatelessWidgetVNode {
+  return isVNode(val) && val.kind === NodeKind.STATELESS_WIDGET
 }
 /**
  * 判断给定值是否为Widget节点
  * @param val - 需要判断的值
  * @returns {boolean} 如果值是Widget节点则返回true，否则返回false
  */
-export function isWidgetNode(val: any): val is WidgetNode {
-  // 首先检查值是否为VNode，然后检查其shapeFlags是否为STATEFUL_WIDGET或STATELESS_WIDGET
+export function isWidgetNode(val: any): val is WidgetVNode {
+  // 首先检查值是否为VNode，然后检查其kind是否为STATEFUL_WIDGET或STATELESS_WIDGET
   return (
     isVNode(val) &&
-    (val.shapeFlags === NodeShapeFlags.STATEFUL_WIDGET ||
-      val.shapeFlags === NodeShapeFlags.STATELESS_WIDGET)
+    (val.kind === NodeKind.STATEFUL_WIDGET || val.kind === NodeKind.STATELESS_WIDGET)
   )
 }
-
 /**
  * 检查给定的值是否为片段虚拟节点(FragmentNode)
  *
  * @param val - 需要检查的值
  * @returns {boolean} 如果是片段虚拟节点返回true，否则返回false
  */
-export function isFragmentNode(val: any): val is FragmentNode {
-  return isVNode(val) && val.shapeFlags === NodeShapeFlags.FRAGMENT
+export function isFragmentNode(val: any): val is FragmentVNode {
+  return isVNode(val) && val.kind === NodeKind.FRAGMENT
 }
-
 /**
  * 检查给定的值是否为常规元素节点(RegularElementNode)
  *
  * @param val - 需要检查的值
  * @returns {boolean} 如果是元素虚拟节点返回true，否则返回false
  */
-export function isRegularElementNode(val: any): val is RegularElementNode {
-  return (
-    isVNode(val) &&
-    typeof val.type === 'string' &&
-    val.shapeFlags === NodeShapeFlags.REGULAR_ELEMENT
-  )
+export function isRegularElementNode(val: any): val is RegularElementVNode {
+  return isVNode(val) && typeof val.type === 'string' && val.kind === NodeKind.REGULAR_ELEMENT
 }
-
 /**
  * 检查一个值是否为空元素虚拟节点
  *
@@ -96,18 +77,18 @@ export function isRegularElementNode(val: any): val is RegularElementNode {
  * @returns {boolean} 如果值是空元素虚拟节点则返回true，否则返回false
  */
 export function isVoidElementNode(val: any): val is VoidElementVNode {
-  return isVNode(val) && val.shapeFlags === NodeShapeFlags.VOID_ELEMENT
+  return isVNode(val) && val.kind === NodeKind.VOID_ELEMENT
 }
 /**
  * 判断给定的值是否为元素节点
  * @param val 需要判断的值
  * @returns {boolean} 如果是元素节点返回true，否则返回false
  */
-export function isElementNode(val: any): val is ElementNode {
+export function isElementNode(val: any): val is ElementVNode {
   return (
     isVNode(val) && // 首先判断是否为虚拟节点
-    (val.shapeFlags === NodeShapeFlags.REGULAR_ELEMENT || // 判断是否为普通元素节点
-      val.shapeFlags === NodeShapeFlags.FRAGMENT) // 或者片段节点
+    (val.kind === NodeKind.REGULAR_ELEMENT || // 判断是否为普通元素节点
+      val.kind === NodeKind.FRAGMENT) // 或者片段节点
   )
 }
 /**
@@ -116,20 +97,18 @@ export function isElementNode(val: any): val is ElementNode {
  * @param val - 要检查的值
  * @returns {boolean} 如果值是文本虚拟节点则返回true，否则返回false
  */
-export function isTextNode(val: any): val is TextNode {
-  return isVNode(val) && val.shapeFlags === NodeShapeFlags.TEXT
+export function isTextNode(val: any): val is TextVNode {
+  return isVNode(val) && val.kind === NodeKind.TEXT
 }
-
 /**
  * 检查给定的值是否为注释虚拟节点(CommentNode)
  *
  * @param val - 需要检查的值
  * @returns {boolean} 如果值是注释虚拟节点则返回true，否则返回false
  */
-export function isCommentNode(val: any): val is CommentNode {
-  return isVNode(val) && val.shapeFlags === NodeShapeFlags.COMMENT
+export function isCommentNode(val: any): val is CommentVNode {
+  return isVNode(val) && val.kind === NodeKind.COMMENT
 }
-
 /**
  * 判断给定的值是否是容器虚拟节点(FragmentNode|RegularElementNode)
  * 容器虚拟节点是指可以包含子节点的虚拟节点
@@ -137,45 +116,15 @@ export function isCommentNode(val: any): val is CommentNode {
  * @param val - 需要检查的值
  * @returns {boolean} 如果值是容器虚拟节点则返回true，否则返回false
  */
-export function isContainerNode(val: any): val is ContainerNode {
-  return (
-    isVNode(val) &&
-    (val.shapeFlags === NodeShapeFlags.REGULAR_ELEMENT ||
-      val.shapeFlags === NodeShapeFlags.FRAGMENT)
-  )
+export function isContainerNode(val: any): val is ContainerVNode {
+  return isVNode(val) && (val.kind === NodeKind.REGULAR_ELEMENT || val.kind === NodeKind.FRAGMENT)
 }
-
 /**
  * 检查给定的值是否为非元素节点(TextNode|CommentNode)
  *
  * @param val - 需要检查的值
  * @returns {boolean} 如果值是非元素节点则返回true，否则返回false
  */
-export function isNonElementNode(val: any): val is TextNode | CommentNode {
-  return (
-    isVNode(val) &&
-    (val.shapeFlags === NodeShapeFlags.TEXT || val.shapeFlags === NodeShapeFlags.COMMENT)
-  )
-}
-
-/**
- * 判断是否支持子节点
- *
- * @param type - 节点类型
- * @returns {boolean} 是否支持子节点
- */
-export const isSupportChildren = (type: ValidNodeType): boolean => {
-  if (typeof type === 'string') {
-    switch (type) {
-      case COMMENT_NODE_TYPE:
-      case TEXT_NODE_TYPE:
-        return false
-      case FRAGMENT_NODE_TYPE:
-      case DYNAMIC_RENDER_TYPE:
-        return true
-      default:
-        return !useDomAdapter().isVoidElement(type)
-    }
-  }
-  return true
+export function isNonElementNode(val: any): val is TextVNode | CommentVNode {
+  return isVNode(val) && (val.kind === NodeKind.TEXT || val.kind === NodeKind.COMMENT)
 }
