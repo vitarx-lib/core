913b503aa8608de4d9b404a14e613103c1c8fe0a
ZhuChongLin <8210856@qq.com>
2025-11-19 18:21:52 +0800

    refactor(runtime-core): 移除 utils 的 dev.js 导出
    - 删除了对 dev.js 的导出，减少无用代码依赖
    - 清理 runtime-core utils 相关模块引用
    - 优化包体积，提升加载性能

--- packages/runtime-core/src/utils/index.ts
+++ packages/runtime-core/src/utils/index.ts
@@ -1,7 +1,6 @@
 export * from './id.js'
 export * from './props.js'
 export * from './style.js'
-export * from './dev.js'
 export * from './ref.js'
 export * from './vnode.js'
 export * from './widget.js'
