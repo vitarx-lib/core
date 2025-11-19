3364482ba75418684e6a3c0c67f94cd4339f4ed9
ZhuChongLin <8210856@qq.com>
2025-11-19 17:36:20 +0800

    feat(runtime-core): 创建基础虚拟节点函数 createBaseVNode
    - 定义 createBaseVNode 函数用于创建基础虚拟节点
    - 支持节点类型、节点类别、属性以及自动解除 ref 响应式引用
    - 处理 props 中的 ref，key，v-static 特殊属性
    - 支持对 v-bind 属性的合并及展开
    - 对不支持 ref 的节点类型进行警告提示
    - 开发环境下附加节点开发信息方便调试
    - 使用 unref 自动处理响应式属性的解除引用

---
+++ packages/runtime-core/src/vnode/baseCreateor.ts
@@ -0,0 +1,78 @@
+import { NON_SIGNAL_SYMBOL, unref } from '@vitarx/responsive'
+import { isObject, logger, popProperty } from '@vitarx/utils'
+import { NodeKind, NodeState, VIRTUAL_NODE_SYMBOL } from '../constants/index.js'
+import { __DEV__, popNodeDevInfo } from '../internal/dev.js'
+import type { AnyProps, VNode, VNodeTypes } from '../types/index.js'
+import { isRefEl } from '../utils/index.js'
+import { bindProps } from './propsNormalizer.js'
+
+// 支持 ref 的节点类型
+const nonSupportRefKind = new Set([
+  NodeKind.TEXT,
+  NodeKind.COMMENT,
+  NodeKind.FRAGMENT,
+  NodeKind.STATELESS_WIDGET
+])
+/**
+ * 创建基础的虚拟节点
+ *
+ * @param type
+ * @param kind
+ * @param props
+ * @param autoUnref
+ */
+export const createBaseVNode = (
+  type: VNodeTypes,
+  kind: NodeKind,
+  props: AnyProps,
+  autoUnref: boolean = true
+): VNode => {
+  props = { ...props }
+  const node: VNode = {
+    [NON_SIGNAL_SYMBOL]: true,
+    [VIRTUAL_NODE_SYMBOL]: true,
+    type,
+    kind,
+    props,
+    state: NodeState.Created
+  }
+
+  // --------- 开发信息（可选） ----------
+  if (__DEV__) {
+    const devInfo = popNodeDevInfo(props)
+    if (devInfo) node.devInfo = devInfo
+  }
+  // 无 props 直接返回
+  if (!Object.keys(props).length) return node
+  // --------- 提取 ref ---------
+  if ('ref' in props) {
+    if (nonSupportRefKind.has(kind)) {
+      logger.warn(`ref is not supported for ${node.type} node`, node.devInfo?.source)
+    } else {
+      const ref = popProperty(props, 'ref')
+      if (isRefEl(ref)) {
+        node.ref = ref
+      } else if (__DEV__) {
+        logger.warn(
+          `ref value is not valid and must be a RefSignal type object`,
+          node.devInfo?.source
+        )
+      }
+    }
+  }
+  // --------- 提取 key ---------
+  if ('key' in props) {
+    node.key = popProperty(props, 'key')
+  }
+  // --------- 特殊属性：v-static ---------
+  if ('v-static' in props) node.static = popProperty(props, 'v-static')
+  // --------- 执行 v-bind 合并 ---------
+  const bind = popProperty(props, 'v-bind')
+  if (isObject(bind)) bindProps(props, bind)
+  if (autoUnref) {
+    for (const [key, value] of Object.entries(props)) {
+      props[key] = unref(value)
+    }
+  }
+  return node
+}
