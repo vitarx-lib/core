fb41a4ba62c6cd2e05e3cdb7eb42004f4e797ad7
ZhuChongLin <8210856@qq.com>
2025-11-21 00:07:24 +0800

    feat(runtime-core): 添加控制器模块导出
    - 新增基类控制器的统一导出
    - 添加元素控制器的统一导出
    - 添加组件控制器的统一导出
    - 方便外部模块统一引用各类控制器组件

---
+++ packages/runtime-core/src/controllers/index.ts
@@ -0,0 +1,17 @@
+// 抽象基类
+export * from './BaseWidgetController.js'
+export * from './ContainerController.js'
+export * from './ElementController.js'
+export * from './HostNodeController.js'
+export * from './NonElementController.js'
+
+// 元素控制器
+export * from './RegularElementController.js'
+export * from './VoidElementController.js'
+export * from './FragmentController.js'
+export * from './TextController.js'
+export * from './CommentController.js'
+
+// 组件控制器
+export * from './StatefulWidgetController.js'
+export * from './StatelessWidgetController.js'
