d8b18809dfb76e3457a62dc3644f48cf35d4e191
ZhuChongLin <8210856@qq.com>
2025-11-27 18:43:11 +0800

    fix(runtime-core): 修复特殊节点的 ref 支持和指令解析逻辑
    - 引入 SPECIAL_NODE_KINDS 常量区分特殊节点类型
    - 修正 ref 判断逻辑，禁止特殊节点使用 ref 并打印警告
    - 增加对特殊节点属性中以 v- 开头指令的解析和绑定机制
    - 优化 v-bind 合并后对属性的处理逻辑
    - 确保指令解析仅对特殊节点生效，避免误用
    - 增加类型导入 VNodeDirectives，完善类型支持

--- packages/runtime-core/src/vnode/creator/base.ts
+++ packages/runtime-core/src/vnode/creator/base.ts
@@ -1,13 +1,17 @@
 import { NON_SIGNAL_SYMBOL, unref } from '@vitarx/responsive'
 import { isObject, logger, popProperty } from '@vitarx/utils'
-import { NodeKind, NodeState, VIRTUAL_NODE_SYMBOL } from '../../constants/index.js'
+import {
+  NodeKind,
+  NodeState,
+  SPECIAL_NODE_KINDS,
+  VIRTUAL_NODE_SYMBOL
+} from '../../constants/index.js'
+import { resolveDirective } from '../../directive/index.js'
 import { getAppContext } from '../../runtime/index.js'
-import type { AnyProps, VNode, VNodeTypes } from '../../types/index.js'
+import type { AnyProps, VNode, VNodeDirectives, VNodeTypes } from '../../types/index.js'
 import { __DEV__, isRefEl, popNodeDevInfo } from '../../utils/index.js'
 import { bindProps } from '../normalizer/props.js'
 
-// 支持 ref 的节点类型
-const nonSupportRefKind = new Set([NodeKind.TEXT, NodeKind.COMMENT, NodeKind.FRAGMENT])
 /**
  * 创建基础的虚拟节点
  *
@@ -36,7 +40,7 @@ export const createBaseVNode = (type: VNodeTypes, kind: NodeKind, props: AnyProp
   if (!Object.keys(props).length) return node
   // --------- 提取 ref ---------
   if ('ref' in props) {
-    if (nonSupportRefKind.has(kind)) {
+    if (SPECIAL_NODE_KINDS.has(kind)) {
       logger.warn(`ref is not supported for ${node.type} node`, node.devInfo?.source)
     } else {
       const ref = popProperty(props, 'ref')
@@ -59,8 +63,29 @@ export const createBaseVNode = (type: VNodeTypes, kind: NodeKind, props: AnyProp
   // --------- 执行 v-bind 合并 ---------
   const bind = popProperty(props, 'v-bind')
   if (isObject(bind)) bindProps(props, bind)
-  for (const [key, value] of Object.entries(props)) {
-    props[key] = unref(value)
+  if (SPECIAL_NODE_KINDS.has(kind)) {
+    const directives: VNodeDirectives = new Map()
+    for (const [key, value] of Object.entries(props)) {
+      props[key] = unref(value)
+      if (key.startsWith('v-') && !SPECIAL_NODE_KINDS.has(kind)) {
+        // 删除 v- 前缀
+        const raw = key.slice(2)
+        // 解析参数（只支持一个冒号）
+        const [name, arg] = raw.split(':', 2)
+        // 查找指令
+        const directive = resolveDirective(name)
+        if (directive) {
+          directives.set(name, [directive, props[key], arg])
+          delete props[key]
+        }
+      }
+    }
+    if (directives.size) node.directives = directives
+  } else {
+    for (const [key, value] of Object.entries(props)) {
+      props[key] = unref(value)
+    }
   }
+
   return node
 }
