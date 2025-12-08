805a24e4ac6cfeccb6c1669dda5062e11230eca0
ZhuChongLin <8210856@qq.com>
2025-12-08 21:00:28 +0800

    feat(runtime-core): 添加版本号设置功能
    - 将版本号初始值改为 'NOT_INITIALIZED'
    - 新增 setVitarxVersion 方法用于设置版本号
    - 版本号由 vitarx 主包自动设置，无需外部调用

--- packages/runtime-core/src/constants/version.ts
+++ packages/runtime-core/src/constants/version.ts
@@ -1,1 +1,9 @@
-export const __VITARX_VERSION__ = '__VERSION__'
+export let __VITARX_VERSION__ = 'NOT_INITIALIZED'
+/**
+ * 设置版本号
+ *
+ * 版本号由 vitarx 主包自动设置，无需外部调用
+ *
+ * @param v - 版本号
+ */
+export const setVitarxVersion = (v: string) => (__VITARX_VERSION__ = v)
