01ac36b8ee28467035ec06f2c64a55a1fb8901f4
ZhuChongLin <8210856@qq.com>
2025-12-14 00:34:17 +0800

    feat(signal): 添加函数信号类型判断功能
    - 新增 IS_FN_SIGNAL 符号以标识函数式 signal
    - 在创建函数信号时添加 IS_FN_SIGNAL 属性
    - 实现 isFnSignal 函数判断值是否为函数信号类型
    - 增强信号系统中函数信号的识别和处理能力

--- packages/responsive/src/signal/core/signal.ts
+++ packages/responsive/src/signal/core/signal.ts
@@ -3,6 +3,10 @@ import { trackSignal, triggerSignal } from '../depend/index.js'
 import { IS_SIGNAL, SIGNAL_RAW_VALUE, SIGNAL_READ_VALUE } from './symbol.js'
 
 /**
+ * 函数式 signal 访问（用于 readFnSignal 读取值，需触发跟踪）
+ */
+const IS_FN_SIGNAL = Symbol.for('__v_is-fn-signal')
+/**
  * FnSignal 函数类型重载：
  * - 无参调用：返回当前值（get）
  * - 有参调用：设置新值（set），返回 void
@@ -40,5 +44,17 @@ export function signal<T>(initialValue: T, options: SignalOptions = {}): FnSigna
   Object.defineProperty(sig, IS_SIGNAL, { value: true })
   Object.defineProperty(sig, SIGNAL_RAW_VALUE, { get: () => _value })
   Object.defineProperty(sig, SIGNAL_READ_VALUE, { get: () => sig() })
+  Object.defineProperty(sig, IS_FN_SIGNAL, { value: true })
   return sig
+}
+
+/**
+ * 判断一个值是否为函数信号(FnSignal)类型
+ *
+ * @param val - 需要检查的值
+ * @returns {boolean} 如果值是FnSignal类型返回true，否则返回false
+ */
+export function isFnSignal(val: any): val is FnSignal {
+  // 双重非运算确保val为真，并且检查IS_FN_SIGNAL属性是否存在
+  return !!val && !!val[IS_FN_SIGNAL]
 }
