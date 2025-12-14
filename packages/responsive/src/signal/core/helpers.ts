82cc47c690b17ec072fbc1fc02a01acccc0d8334
ZhuChongLin <8210856@qq.com>
2025-12-15 03:37:46 +0800

    refactor(signal): 优化信号值读写和非信号标记相关辅助函数
    - 导入并使用新的信号值符号 SIGNAL_VALUE，替代 SIGNAL_RAW_VALUE 读取信号值
    - 新增 writeSignal 函数，用于写入信号值并触发信号
    - 将 readSignal 和 writeSignal 分别导出为 getSignal 和 setSignal 以提供别名
    - 修改 markNonSignal 函数使用 IS_NON_SIGNAL 标识代替以前的 NON_SIGNAL
    - 新增 isMarkNonSignal 函数检查对象是否被标记为非信号类型
    - 调整相关注释，统一说明信号值访问与标记行为

--- packages/responsive/src/signal/core/helpers.ts
+++ packages/responsive/src/signal/core/helpers.ts
@@ -1,6 +1,6 @@
 import { AnyObject, isObject } from '@vitarx/utils'
 import type { NonSignal, Signal } from '../../types/index.js'
-import { IS_SIGNAL, NON_SIGNAL, SIGNAL_RAW_VALUE } from './symbol.js'
+import { IS_NON_SIGNAL, IS_SIGNAL, SIGNAL_RAW_VALUE, SIGNAL_VALUE } from './symbol.js'
 
 /**
  * 判断对象是否是 Signal
@@ -9,7 +9,7 @@ export function isSignal(obj: any): obj is Signal {
   return !!obj && !!obj[IS_SIGNAL]
 }
 /**
- * 获取信号原始值，不触发依赖收集
+ * 获取信号原始值 - 不触发跟踪
  *
  * @param sig - 信号对象
  * @returns - 信号原始值
@@ -21,16 +21,28 @@ export function toRaw<T>(sig: T): T extends Signal<T> ? T : T {
 }
 
 /**
- * 获取信号值并触发依赖收集
+ * 获取信号值的辅助函数 - 跟踪信号
  *
+ * @alias getSignal
  * @param sig - 信号对象
- * @returns - 信号值
+ * @returns {any} - 如果是信号对象，则返回信号值；否则返回原值
  */
 export function readSignal<T>(sig: Signal<T>): T extends Signal<T> ? T : T {
   if (!isSignal(sig)) return sig
-  if (SIGNAL_RAW_VALUE in sig) return sig[SIGNAL_RAW_VALUE] as any
+  if (SIGNAL_VALUE in sig) return sig[SIGNAL_VALUE] as any
   return sig as any
 }
+/**
+ * 写入信号值的辅助函数 - 触发信号
+ *
+ * @alias setSignal
+ * @param sig - 要设置的信号对象
+ * @param value - 要设置的新值
+ */
+export function writeSignal<T>(sig: Signal<T>, value: T): void {
+  sig[SIGNAL_VALUE] = value
+}
+export { readSignal as getSignal, writeSignal as setSignal }
 
 /**
  * 将一个对象标记为永远不会被转换为响应式信号。
@@ -51,14 +63,28 @@ export function readSignal<T>(sig: Signal<T>): T extends Signal<T> ? T : T {
  * console.log(isSignal(signal)); // false
  * ```
  */
-export function markNonSignal<T extends AnyObject>(obj: T): NonSignal<T> {
+export function markNonSignal<T extends object>(obj: T): NonSignal<T> {
   if (!isObject(obj)) {
     throw new TypeError('[markNonSignal]: The argument must be an object type')
   }
-  Object.defineProperty(obj, NON_SIGNAL, {
+  Object.defineProperty(obj, IS_NON_SIGNAL, {
     value: true
   })
   return obj as NonSignal<T>
 }
 
 export { markNonSignal as markRaw }
+
+/**
+ * 检查对象是否被标记为非信号类型
+ *
+ * @param obj - 需要检查的对象
+ * @returns {boolean} 如果对象存在且具有NON_SIGNAL属性则返回true，否则返回false
+ */
+export function isMarkNonSignal(obj: any): boolean {
+  // 使用!!操作符将对象和其NON_SIGNAL属性转换为布尔值
+  // 只有当对象和其NON_SIGNAL属性都存在时才返回true
+  return !!obj && !!obj[IS_NON_SIGNAL]
+}
+
+export { isMarkNonSignal as isRaw }
