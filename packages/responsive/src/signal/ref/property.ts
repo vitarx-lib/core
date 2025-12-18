895a6b36328ac5a04a6c17722eb7fc3a02ed0ec1
ZhuChongLin <8210856@qq.com>
2025-12-18 17:24:29 +0800

    refactor(types): 更新 PropertyRef 实现以使用 RefWrapper 接口
    - 将 PropertyRef 类实现从 WritableRef 更新为 RefWrapper
    - 修改相关导入类型以匹配新的接口定义
    - 保持 PropertyRef 的泛型和功能不变，只更换类型契约名称

--- packages/responsive/src/signal/ref/property.ts
+++ packages/responsive/src/signal/ref/property.ts
@@ -1,5 +1,5 @@
 import { REF_SYMBOL } from '../../constants/index.js'
-import type { WritableRef } from '../../types/index.js'
+import type { RefWrapper } from '../../types/index.js'
 
 /**
  * PropertyRef 是一个泛型类，用于对象属性的引用。
@@ -32,7 +32,7 @@ import type { WritableRef } from '../../types/index.js'
  * - 属性键必须是目标对象的有效键
  * - 当属性值为 undefined 时，将返回默认值（如果提供）
  */
-export class PropertyRef<T extends object, K extends keyof T> implements WritableRef<T[K]> {
+export class PropertyRef<T extends object, K extends keyof T> implements RefWrapper<T[K]> {
   constructor(
     private readonly _target: T,
     private readonly _key: K,
