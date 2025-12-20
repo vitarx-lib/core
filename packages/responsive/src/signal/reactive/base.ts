dbe6876bd85025fc3b30a96e674f24b9f2fb984a
ZhuChongLin <8210856@qq.com>
2025-12-20 22:59:41 +0800

    refactor(reactive): 替换信号调试数据类型为 ExtraDebugData
    - 将 triggerSignal 方法中的 devInfo 参数类型由 DebuggerEventOptions 修改为 ExtraDebugData
    - 将 trackSignal 方法中的 devInfo 参数类型由 DebuggerEventOptions 修改为 ExtraDebugData
    - 保持开发环境下的信号触发和跟踪逻辑不变，仅更新类型定义以增强调试数据扩展性

--- packages/responsive/src/signal/reactive/base.ts
+++ packages/responsive/src/signal/reactive/base.ts
@@ -1,6 +1,6 @@
 import { IS_REACTIVE, IS_REF, IS_SIGNAL } from '../../constants/index.js'
 import { trackSignal, triggerSignal } from '../../depend/index.js'
-import type { DebuggerEventOptions, Reactive, RefSignal, SignalOpType } from '../../types/index.js'
+import type { ExtraDebugData, Reactive, RefSignal, SignalOpType } from '../../types/index.js'
 
 /**
  * ReactiveSource 是一个抽象类，用于创建响应式对象代理。
@@ -45,7 +45,7 @@ export abstract class ReactiveSource<T extends object, Deep extends boolean = tr
    * @param type - 信号操作类型
    * @param devInfo - 调试器事件选项（可选参数，仅在开发环境使用）
    */
-  protected triggerSignal(type: SignalOpType, devInfo?: DebuggerEventOptions) {
+  protected triggerSignal(type: SignalOpType, devInfo?: ExtraDebugData) {
     this.version++
     // 判断是否为开发环境
     if (__DEV__) {
@@ -62,7 +62,7 @@ export abstract class ReactiveSource<T extends object, Deep extends boolean = tr
    * @param devInfo - 可选参数，设备信息对象，包含设备的额外信息
    * 该方法用于跟踪与代理对象相关的信号操作，将相关信息传递给trackSignal函数
    */
-  protected trackSignal(type: SignalOpType, devInfo?: DebuggerEventOptions) {
+  protected trackSignal(type: SignalOpType, devInfo?: ExtraDebugData) {
     trackSignal(this, type, devInfo) // 调用trackSignal函数，传入代理对象、操作类型和设备信息
   }
   /**
