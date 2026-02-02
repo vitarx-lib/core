c0d41ddde360bb3a5efd062d21101473eae886c9
ZhuChongLin <8210856@qq.com>
2026-02-03 00:50:45 +0800

    fix(runtime-core): 修复视图类型判断函数返回值类型
    - 将所有视图类型判断函数的返回值转换为布尔值，避免返回非布尔值
    - 修正isSwitchView函数名称为isDynamicView，保持命名和类型一致
    - 更新相关函数的注释，准确描述返回值含义
    - 优化代码可读性，保证判定表达式明确返回boolean类型

--- packages/runtime-core/src/shared/utils/is.ts
+++ packages/runtime-core/src/shared/utils/is.ts
@@ -29,17 +29,17 @@ export const isView = (val: any): val is View => {
  * @returns {boolean} 如果值是ComponentView组件视图类型则返回true，否则返回false
  */
 export const isComponentView = (val: any): val is ComponentView => {
-  return val?.[IS_VIEW] && val.kind === ViewKind.COMPONENT
+  return !!(val?.[IS_VIEW] && val.kind === ViewKind.COMPONENT)
 }
 
 /**
- * 检查给定值是否为SwitchView条件视图类型
+ * 检查给定值是否为DynamicView条件视图类型
  *
  * @param val - 需要检查的任意值
- * @returns {boolean} 如果值是SwitchView条件视图类型则返回true，否则返回false
+ * @returns {boolean} 如果值是DynamicView条件视图类型则返回true，否则返回false
  */
-export const isSwitchView = (val: any): val is DynamicView => {
-  return val?.[IS_VIEW] && val.kind === ViewKind.DYNAMIC
+export const isDynamicView = (val: any): val is DynamicView => {
+  return !!(val?.[IS_VIEW] && val.kind === ViewKind.DYNAMIC)
 }
 
 /**
@@ -49,7 +49,7 @@ export const isSwitchView = (val: any): val is DynamicView => {
  * @returns {boolean} 如果值是TextView文本视图类型则返回true，否则返回false
  */
 export const isTextView = (val: any): val is TextView => {
-  return val?.[IS_VIEW] && val.kind === ViewKind.TEXT
+  return !!(val?.[IS_VIEW] && val.kind === ViewKind.TEXT)
 }
 
 /**
@@ -59,7 +59,7 @@ export const isTextView = (val: any): val is TextView => {
  * @returns {boolean} 如果值是CommentView注释视图类型则返回true，否则返回false
  */
 export const isCommentView = (val: any): val is CommentView => {
-  return val?.[IS_VIEW] && val.kind === ViewKind.COMMENT
+  return !!(val?.[IS_VIEW] && val.kind === ViewKind.COMMENT)
 }
 
 /**
@@ -69,7 +69,7 @@ export const isCommentView = (val: any): val is CommentView => {
  * @returns {boolean} 如果值是ElementView元素视图类型则返回true，否则返回false
  */
 export const isElementView = (val: any): val is ElementView => {
-  return val?.[IS_VIEW] && val.kind === ViewKind.ELEMENT
+  return !!(val?.[IS_VIEW] && val.kind === ViewKind.ELEMENT)
 }
 
 /**
@@ -79,7 +79,7 @@ export const isElementView = (val: any): val is ElementView => {
  * @returns {boolean} 如果值是FragmentView片段视图类型则返回true，否则返回false
  */
 export const isFragmentView = (val: any): val is FragmentView => {
-  return val?.[IS_VIEW] && val.kind === ViewKind.FRAGMENT
+  return !!(val?.[IS_VIEW] && val.kind === ViewKind.FRAGMENT)
 }
 
 /**
@@ -89,7 +89,7 @@ export const isFragmentView = (val: any): val is FragmentView => {
  * @return {boolean} 如果值是ListView列表视图类型则返回true，否则返回false
  */
 export const isListView = (val: any): val is ListView => {
-  return val?.[IS_VIEW] && val.kind === ViewKind.LIST
+  return !!(val?.[IS_VIEW] && val.kind === ViewKind.LIST)
 }
 
 /**
