39bd769488722b0417c514695f1f0dac2ca87327
ZhuChongLin <8210856@qq.com>
2026-02-11 02:39:17 +0800

    feat(runtime-dom): 注册 show、html 和 text 指令
    - 从 runtime-core 导入 defineDirective 方法
    - 引入 show、html 和 text 指令模块
    - 使用 defineDirective 注册 show 指令
    - 使用 defineDirective 注册 html 指令
    - 使用 defineDirective 注册 text 指令

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,2 +1,10 @@
+import { defineDirective } from '@vitarx/runtime-core'
+import html from './directives/html.js'
+import show from './directives/show.js'
+import text from './directives/text.js'
+
 export * from './core/index.js'
 export type * from './types/index.js'
+defineDirective('show', show)
+defineDirective('html', html)
+defineDirective('text', text)
