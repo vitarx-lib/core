3ede73776e96690c2fb29fdaa02d4afd3b3f0b34
ZhuChongLin <8210856@qq.com>
2026-02-12 05:37:21 +0800

    feat(vitarx): 注入并导出运行时版本信息
    - 从 runtime-core 导入 App
    - 在打包后动态注入版本号到 App.version
    - 保持其他模块的导出不变
    - 添加类型忽略提示避免类型检查错误
    - 增强版本管理的可维护性和透明度

--- packages/vitarx/src/index.ts
+++ packages/vitarx/src/index.ts
@@ -1,5 +1,11 @@
+import { App } from '@vitarx/runtime-core'
+
 export * from '@vitarx/utils'
 export * from '@vitarx/responsive'
 export * from '@vitarx/runtime-core'
 export * from '@vitarx/runtime-dom'
 export * from '@vitarx/runtime-ssr'
+
+// @ts-ignore 打包后注入
+// noinspection JSConstantReassignment
+App.version = '__VERSION__'
