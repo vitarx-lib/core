470bbc6607dad5d27a1ec94766f1ff1398224638
ZhuChongLin <8210856@qq.com>
2026-02-13 00:54:40 +0800

    test(performance): 删除不必要的调试代码
    - 移除未使用的 getStackTrace 导入
    - 删除测试文件中相关的调试打印语句
    - 减少测试代码冗余，提高性能测试的干净度

--- packages/responsive/tests/performance/arr.ts
+++ packages/responsive/tests/performance/arr.ts
@@ -1,5 +1,4 @@
 ;(globalThis as any).__DEV__ = false
-import { getStackTrace } from '@vitarx/utils/src/index.js'
 import { ref, watch } from '../../src/index.js'
 
 const arr = ref([1, 2, 3])
@@ -8,5 +7,3 @@ watch(arr.value, (newValue, oldValue) => {
   console.log(newValue, oldValue)
 })
 arr.value.reverse()
-
-console.log(getStackTrace())
